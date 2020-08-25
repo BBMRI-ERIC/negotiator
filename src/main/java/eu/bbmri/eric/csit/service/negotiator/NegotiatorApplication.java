9e0197989611a1977fa065e7ce379371394718f4
reihsr <robert.reihs@gmail.com>
2020-08-25 13:41:44 +0200

    Initial commit for Negotiator v3.x

---
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/NegotiatorApplication.java
@@ -0,0 +1,13 @@
+package eu.bbmri.eric.csit.service.negotiator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegotiatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NegotiatorApplication.class, args);
+    }
+
+}
