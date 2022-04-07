bb4ebf24563dfc36dfc72a1a5036bfc337feda45
Vittorio Meloni <vittorio.meloni@crs4.it>
2022-04-07 17:34:03 +0200

    feat: adds very firsts security configuration

---
+++ application/src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package eu.bbmri.eric.csit.service.negotiator.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    //    http.authorizeRequests(
+    //            authz ->
+    //                authz
+    //                    .antMatchers(HttpMethod.GET, "/data-sources/**")
+    //                    .permitAll()
+    //                    .anyRequest()
+    //                    .authenticated())
+    //        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    http.cors().and().csrf().disable();
+  }
+}
