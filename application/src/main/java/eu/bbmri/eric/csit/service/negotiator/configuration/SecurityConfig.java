a7315c2945262898073b2fed0c1ea3981e5415a3
Vittorio Meloni <vittorio.meloni@crs4.it>
2022-04-21 10:02:03 +0200

    WIP: adds support to basic auth and jwt. Need some work on how to use them together

--- application/src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/SecurityConfig.java
+++ application/src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/SecurityConfig.java
@@ -4,26 +4,63 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@Profile({"dev", "prod"})
+@Profile({"devlocal", "prod"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("admin")
+        .password(passwordEncoder().encode("admin"))
+        .authorities("SCOPE_openid")
+        .roles("ADMIN");
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    //    http.authorizeRequests(
-    //            authz ->
-    //                authz
-    //                    .antMatchers(HttpMethod.GET, "/data-sources/**")
-    //                    .permitAll()
-    //                    .anyRequest()
-    //                    .authenticated())
-    //        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-    http.cors().and().csrf().disable();
+    http.sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .cors()
+        .and()
+        .csrf()
+        .disable();
+
+    http.authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/v3/projects/**")
+        .hasAuthority("SCOPE_openid")
+        .and()
+
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/v3/queries/**")
+        .authenticated()
+
+        .and()
+        .authorizeRequests()
+        .anyRequest()
+        .permitAll()
+
+        .and()
+        .httpBasic()
+        .and()
+        .oauth2ResourceServer()
+        .jwt();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
