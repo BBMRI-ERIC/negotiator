38d445c366bdd1513ae79893874e51339f8792c3
Vittorio Meloni <vittorio.meloni@crs4.it>
2022-05-04 11:59:33 +0200

    feat: adds jwt converter to convert from scopes to roles

--- negotiator-application/src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/SecurityConfig.java
+++ negotiator-application/src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/SecurityConfig.java
@@ -1,15 +1,27 @@
 package eu.bbmri.eric.csit.service.negotiator.configuration;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -46,15 +58,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     http.authorizeRequests()
         .antMatchers(HttpMethod.POST, "/v3/data-sources/**")
-        .hasRole("ADMIN")
+        .hasAuthority("ADMIN")
         .and()
         .authorizeRequests()
         .antMatchers("/v3/projects/**", "/v3/requests/**")
-        .hasAuthority("SCOPE_openid")
+        .hasAuthority("RESEARCHER")
         .and()
         .authorizeRequests()
         .antMatchers(HttpMethod.POST, "/v3/queries/**")
-        .hasAnyRole("ADMIN", "EXT_SERV")
+        .hasAnyAuthority("ADMIN", "EXT_SERV")
         .and()
         .authorizeRequests()
         .anyRequest()
@@ -63,6 +75,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .httpBasic()
         .and()
         .oauth2ResourceServer()
-        .jwt();
+        .jwt()
+        .jwtAuthenticationConverter(getJwtAuthenticationConverter());
+  }
+
+  private Converter<Jwt, AbstractAuthenticationToken> getJwtAuthenticationConverter() {
+    var converter = new JwtAuthenticationConverter();
+    converter.setJwtGrantedAuthoritiesConverter(
+        jwt -> {
+          Collection<GrantedAuthority> authority = new HashSet<>();
+          if (jwt.getClaims().containsKey("scope")) {
+            String[] scopes = jwt.getClaims().get("scope").toString().split(" ");
+            if (Arrays.asList(scopes).contains("openid")) {
+              authority.add(new SimpleGrantedAuthority("RESEARCHERs"));
+            }
+          }
+          return authority;
+        });
+    return converter;
   }
 }
