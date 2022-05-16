a8b6f82473b21c9e0c87385b197923b662eb8b9b
Vittorio Meloni <vittorio.meloni@crs4.it>
2022-05-16 12:47:06 +0200

    chore: adds basic auth configuration

---
+++ negotiator-application/src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/BasicSecurityConfig.java
@@ -0,0 +1,75 @@
+package eu.bbmri.eric.csit.service.negotiator.configuration;
+
+import eu.bbmri.eric.csit.service.negotiator.repository.PersonRepository;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@Profile({"docker"})
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired public NegotiatorUserDetailsService userDetailsService;
+
+  @Autowired public DataSource dataSource;
+
+  @Autowired public PasswordEncoder passwordEncoder;
+
+  @Autowired public PersonRepository personRepository;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder)
+        .and()
+        .jdbcAuthentication()
+        .dataSource(dataSource);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .cors()
+        .and()
+        .csrf()
+        .disable();
+
+    http.authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/v3/data-sources/**")
+        .hasAuthority("ADMIN")
+        .and()
+        .authorizeRequests()
+        .antMatchers("/v3/projects/**", "/v3/requests/**")
+        .hasAuthority("RESEARCHER")
+        .and()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/v3/queries/**")
+        .hasAnyAuthority("ADMIN", "EXT_SERV")
+        .and()
+        .authorizeRequests()
+        .antMatchers("/perun/**")
+        .hasAuthority("PERUN_USER")
+        .and()
+        .authorizeRequests()
+        .anyRequest()
+        .permitAll()
+        .and()
+        .httpBasic();
+    //        .and()
+    //        .oauth2ResourceServer()
+    //        .jwt()
+    //        .jwtAuthenticationConverter(new
+    // NegotiatorJwtAuthenticationConverter(personRepository));
+  }
+}
