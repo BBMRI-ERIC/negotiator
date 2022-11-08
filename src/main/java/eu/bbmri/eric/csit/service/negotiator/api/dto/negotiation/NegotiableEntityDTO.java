38ba9e80eb4644f423126b19365d122fbd45d233
RadovanTomik <tomasik@mail.muni.cz>
2022-11-08 09:48:55 +0100

    entity dto

---
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/api/dto/negotiation/NegotiableEntityDTO.java
@@ -0,0 +1,26 @@
+package eu.bbmri.eric.csit.service.negotiator.api.dto.negotiation;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import eu.bbmri.eric.csit.service.negotiator.api.dto.query.ResourceDTO;
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+public abstract class NegotiableEntityDTO {
+    @NotNull
+    private String entity_identifier;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private Set<NegotiableEntityDTO> children;
+}
