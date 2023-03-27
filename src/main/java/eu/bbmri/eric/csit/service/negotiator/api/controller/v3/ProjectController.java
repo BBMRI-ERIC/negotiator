ce04246d53885da7e301fcb78b678fd040012590
RadovanTomik <tomasik@mail.muni.cz>
2023-03-27 11:11:05 +0200

    Fix import grouping

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/api/controller/v3/ProjectController.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/api/controller/v3/ProjectController.java
@@ -11,7 +11,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v3")
--- src/main/java/eu/bbmri/eric/csit/service/negotiator/api/controller/v3/RequestController.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/api/controller/v3/RequestController.java
@@ -19,7 +19,15 @@ import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v3")
