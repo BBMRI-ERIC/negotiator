11063378de2b6d2d2efdc301466f280fb4aefd76
RadovanTomik <tomasik@mail.muni.cz>
2023-04-17 15:37:44 +0200

    Add NegotiationStateMachineConfig

---
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/NegotiationStateMachineConfig.java
@@ -0,0 +1,79 @@
+package eu.bbmri.eric.csit.service.negotiator.configuration;
+
+import eu.bbmri.eric.csit.service.negotiator.database.model.NegotiationEvent;
+import eu.bbmri.eric.csit.service.negotiator.database.model.NegotiationState;
+import lombok.extern.java.Log;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.data.jpa.JpaPersistingStateMachineInterceptor;
+import org.springframework.statemachine.data.jpa.JpaStateMachineRepository;
+import org.springframework.statemachine.listener.StateMachineListener;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.persist.StateMachineRuntimePersister;
+import org.springframework.statemachine.service.DefaultStateMachineService;
+import org.springframework.statemachine.service.StateMachineService;
+import org.springframework.statemachine.state.State;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachineFactory
+@Log
+public class NegotiationStateMachineConfig extends EnumStateMachineConfigurerAdapter<NegotiationState, NegotiationEvent> {
+
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<NegotiationState, NegotiationEvent> config)
+            throws Exception {
+        config
+                .withConfiguration()
+                .autoStartup(true)
+                .listener(listener());
+    }
+
+    @Override
+    public void configure(StateMachineStateConfigurer<NegotiationState, NegotiationEvent> states)
+            throws Exception {
+        states
+                .withStates()
+                .initial(NegotiationState.SUBMITTED)
+                .states(EnumSet.allOf(NegotiationState.class));
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<NegotiationState, NegotiationEvent> transitions)
+            throws Exception {
+        transitions
+                .withExternal()
+                .source(NegotiationState.SUBMITTED).target(NegotiationState.APPROVED).event(NegotiationEvent.APPROVE);
+    }
+
+    @Bean
+    public StateMachineListener<NegotiationState, NegotiationEvent> listener() {
+        return new StateMachineListenerAdapter<>() {
+            @Override
+            public void stateChanged(State<NegotiationState, NegotiationEvent> from, State<NegotiationState, NegotiationEvent> to) {
+                log.info("State change to " + to.getId());
+            }
+        };
+    }
+
+    @Bean
+    public StateMachineService<NegotiationState, NegotiationEvent> stateMachineService(
+            final StateMachineFactory<NegotiationState, NegotiationEvent> stateMachineFactory,
+            final StateMachineRuntimePersister<NegotiationState, NegotiationEvent, String> stateMachinePersist) {
+        return new DefaultStateMachineService<>(stateMachineFactory, stateMachinePersist);
+    }
+
+    @Bean
+    public StateMachineRuntimePersister<NegotiationState, NegotiationEvent, String> stateMachineRuntimePersister(
+            JpaStateMachineRepository jpaStateMachineRepository) {
+        return new JpaPersistingStateMachineInterceptor<>(jpaStateMachineRepository);
+    }
+}
--- src/main/java/eu/bbmri/eric/csit/service/negotiator/database/model/NegotiationEvents.java
+++
@@ -1,8 +0,0 @@
-package eu.bbmri.eric.csit.service.negotiator.database.model;
-
-public enum NegotiationEvents {
-    APPROVE,
-    START,
-    ABANDON,
-    CONCLUDE
-}
