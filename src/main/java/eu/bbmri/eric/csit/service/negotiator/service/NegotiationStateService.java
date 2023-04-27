77a1b5a4a6f54d4a23af6a159386ad19df263a78
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-04-27 08:59:00 +0200

    Add javadoc for the NegotiationStateService

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NegotiationStateService.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NegotiationStateService.java
@@ -8,9 +8,27 @@ import java.util.List;
 
 @Service
 public interface NegotiationStateService {
+    /**
+     * Returns the current state of a Negotiation
+     * @param negotiationId for which state is requested
+     * @return NegotiationState
+     */
     NegotiationState getNegotiationState(String negotiationId);
 
+    /**
+     * Returns all possible events that can be sent for this negotiation
+     * @param negotiationId of the Negotiation
+     * @return a lists of all possible events
+     */
+
     List<NegotiationEvent> getPossibleEvents(String negotiationId);
+
+    /**
+     * Send an event to a particular negotiation
+     * @param negotiationId of the Negotiation
+     * @param negotiationEvent event being sent to the Negotiation
+     * @return the new status of the Negotiation
+     */
 
     NegotiationState sendEvent(String negotiationId, NegotiationEvent negotiationEvent);
 }
