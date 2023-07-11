ebe102a0c818d496e540b712bf9cd68d7d8f795a
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-07-11 10:40:22 +0200

    Add basic Person unit tests

---
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/model/PersonTest.java
@@ -0,0 +1,19 @@
+package eu.bbmri.eric.csit.service.negotiator.unit.model;
+
+import eu.bbmri.eric.csit.service.negotiator.database.model.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest {
+  @Test
+  void testInit() {
+      assertInstanceOf(Person.class, new Person());
+  }
+
+  @Test
+  void getPersonId() {
+      Person person = new Person();
+      assertNull(person.getId());
+  }
+}
