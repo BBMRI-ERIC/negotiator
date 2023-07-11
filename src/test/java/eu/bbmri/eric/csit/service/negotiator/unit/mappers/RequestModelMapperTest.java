b7f5574068e1a36892465901dc6796732a7c8955
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-07-11 17:02:40 +0200

    Add test Request mapper

--- src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/mappers/RequestModelMapperTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/mappers/RequestModelMapperTest.java
@@ -1,11 +1,19 @@
 package eu.bbmri.eric.csit.service.negotiator.unit.mappers;
 
+import eu.bbmri.eric.csit.service.negotiator.database.model.Request;
+import eu.bbmri.eric.csit.service.negotiator.database.model.Resource;
+import eu.bbmri.eric.csit.service.negotiator.dto.request.RequestDTO;
 import eu.bbmri.eric.csit.service.negotiator.mappers.RequestModelsMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.modelmapper.ModelMapper;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RequestModelMapperTest {
     @Spy
@@ -18,5 +26,20 @@ public class RequestModelMapperTest {
     public void setup() {
         MockitoAnnotations.openMocks(this);
         this.requestModelsMapper.addMappings();
+    }
+    @Test
+    void testBasicMapping() {
+        Request request = new Request();
+        request.setId("newRequest");
+        Resource resource = new Resource();
+        resource.setType("biobank");
+        resource.setParent(null);
+        resource.setSourceId("biobank:1");
+        Resource childResource = new Resource();
+        childResource.setType("collection");
+        childResource.setSourceId("collection:1");
+        resource.setChildren(Set.of(childResource));
+        request.setResources(Set.of(resource));
+        assertEquals(request.getId(), this.mapper.map(request, RequestDTO.class).getId());
     }
 }
