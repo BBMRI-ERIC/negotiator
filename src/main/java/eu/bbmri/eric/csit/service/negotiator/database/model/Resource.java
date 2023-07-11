cd6a5e0d7a4e12c273cdf08784fa85efe8ff2e95
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-07-11 17:04:05 +0200

    Remove attributes from Resource

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/database/model/Resource.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/database/model/Resource.java
@@ -2,19 +2,14 @@ package eu.bbmri.eric.csit.service.negotiator.database.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sun.istack.NotNull;
-import com.sun.istack.Nullable;
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedAttributeNode;
 import javax.persistence.NamedEntityGraph;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,13 +28,11 @@ import lombok.ToString.Exclude;
 @Entity(name = "Resource")
 @Table(name = "resource")
 @NamedEntityGraph(
-    name = "resource-with-children",
+    name = "resource",
     attributeNodes = {
       @NamedAttributeNode("sourceId"),
       @NamedAttributeNode("name"),
-      @NamedAttributeNode("type"),
-      @NamedAttributeNode("description"),
-      @NamedAttributeNode("parent")
+      @NamedAttributeNode("description")
     })
 public class Resource extends BaseEntity {
 
@@ -49,22 +42,7 @@ public class Resource extends BaseEntity {
   private String description;
 
   @NotNull private String sourceId;
-
-  @NotNull private String type;
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "parent_id")
-  @Exclude
-  @Nullable
-  private Resource parent;
-
-  @OneToMany(mappedBy = "parent")
-  private Set<Resource> children = new HashSet<>();
-
-  @ManyToMany(mappedBy = "resources")
-  @Exclude
-  private Set<Person> persons = new HashSet<>();
-
+  
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "data_source_id")
   @Exclude
