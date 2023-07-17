a09f42d57cb861f811327124000425defa2b74b6
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-07-17 13:38:09 +0200

    Add tests for Request model mapper

--- src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/mappers/RequestModelMapperTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/mappers/RequestModelMapperTest.java
@@ -1,14 +1,15 @@
 package eu.bbmri.eric.csit.service.negotiator.unit.mappers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import eu.bbmri.eric.csit.service.negotiator.database.model.Request;
 import eu.bbmri.eric.csit.service.negotiator.database.model.Resource;
-import eu.bbmri.eric.csit.service.negotiator.dto.request.RequestCreateDTO;
-import eu.bbmri.eric.csit.service.negotiator.dto.request.RequestDTO;
-import eu.bbmri.eric.csit.service.negotiator.dto.request.ResourceDTO;
+import eu.bbmri.eric.csit.service.negotiator.dto.request.*;
 import eu.bbmri.eric.csit.service.negotiator.mappers.RequestModelsMapper;
 import eu.bbmri.eric.csit.service.negotiator.mappers.ResourceModelMapper;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,7 +33,7 @@ public class RequestModelMapperTest {
   }
 
   @Test
-  void mapRequestToDTO_Ok() {
+  void map_requestToDTO_Ok() {
     Request request = new Request();
     request.setId("newRequest");
     Resource resource = new Resource();
@@ -46,7 +47,7 @@ public class RequestModelMapperTest {
   }
 
   @Test
-  void mapCreateDTOtoRequest_Ok() {
+  void map_createDTOtoRequest_Ok() {
     ResourceDTO resourceDTO =
         ResourceDTO.builder().id("test:collection").name("My collection").build();
     RequestCreateDTO requestCreateDTO =
@@ -61,5 +62,37 @@ public class RequestModelMapperTest {
     assertEquals(
         requestCreateDTO.getResources().iterator().next().getId(),
         request.getResources().iterator().next().getSourceId());
+  }
+
+  @Test
+  void map_v2QueryToRequestCreateDTO_ok() {
+    QueryCreateV2DTO queryCreateV2DTO = buildQueryCreateV2DTO();
+    RequestCreateDTO requestCreateDTO = mapper.map(queryCreateV2DTO, RequestCreateDTO.class);
+    assertEquals(queryCreateV2DTO.getHumanReadable(), requestCreateDTO.getHumanReadable());
+    assertEquals(queryCreateV2DTO.getUrl(), requestCreateDTO.getUrl());
+    queryCreateV2DTO.getCollections().forEach(
+            collectionV2DTO -> assertTrue(requestCreateDTO.getResources().stream()
+                    .anyMatch(collection -> Objects.equals(collection.getId(),
+                            collectionV2DTO.getCollectionId())))
+    );
+    
+  }
+
+  private static QueryCreateV2DTO buildQueryCreateV2DTO() {
+    Set<CollectionV2DTO> collectionV2DTOS = new HashSet<>();
+    collectionV2DTOS.add(CollectionV2DTO.builder().
+            collectionId("collection1")
+                    .biobankId("biobank1")
+            .build());
+    collectionV2DTOS.add(CollectionV2DTO.builder().
+            collectionId("collection2")
+            .biobankId("biobank2")
+            .build());
+    return QueryCreateV2DTO.builder()
+            .url("https://directory.com")
+            .humanReadable("I want everything!")
+            .token("randomlyGeneratedString")
+            .collections(collectionV2DTOS)
+            .build();
   }
 }
