0433b3553bfe7219f0ae80323604fd9a7b17fb5e
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-07-17 13:16:37 +0200

    Add tests for Request equals

--- src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/model/RequestTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/model/RequestTest.java
@@ -1,13 +1,28 @@
 package eu.bbmri.eric.csit.service.negotiator.unit.model;
 
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-
 import eu.bbmri.eric.csit.service.negotiator.database.model.Request;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RequestTest {
   @Test
   void testInitRequest() {
     assertInstanceOf(Request.class, new Request());
+  }
+
+  @Test
+  void equals_sameId_equal() {
+    assertEquals(Request.builder().id("test").build(), Request.builder().id("test").build());
+  }
+
+  @Test
+  void equals_differentId_notEqual() {
+    assertNotEquals(Request.builder().id("test").build(), Request.builder().id("notTest").build());
+  }
+
+  @Test
+  void equals_idIsNull_equal() {
+    assertEquals(new Request(), new Request());
   }
 }
