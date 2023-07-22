c5047e284c804aa9b0330a23e10f7c9d4ffe1035
RadovanTomik <tomasik.radovan@gmail.com>
2023-07-22 13:07:49 +0200

    Add test method for getting possible events

--- src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/SpringStateMachineTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/SpringStateMachineTest.java
@@ -1,16 +1,19 @@
 package eu.bbmri.eric.csit.service.negotiator.integration;
 
-import eu.bbmri.eric.csit.service.negotiator.database.repository.NegotiationRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.apachecommons.CommonsLog;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.recipes.persist.PersistStateMachineHandler;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 enum TestStates {
   SUBMITTED,
@@ -28,21 +31,43 @@ enum TestEvents {
 @ActiveProfiles("test")
 @CommonsLog
 public class SpringStateMachineTest {
+  @Autowired
+  @Qualifier("testMachine")
+  private StateMachine<String, String> entityStateMachine;
 
-    @Autowired
-    @Qualifier("testHandler")
-    private PersistStateMachineHandler persistStateMachineHandler;
-
-    @Autowired
-    private NegotiationRepository negotiationRepository;
+  @Autowired
+  @Qualifier("testHandler")
+  private PersistStateMachineHandler persistStateMachineHandler;
 
   @Test
   void init() {
-      long count = negotiationRepository.count();
-      log.info(count);
-      persistStateMachineHandler.handleEventWithStateReactively(MessageBuilder.
-                      withPayload(TestEvents.APPROVE.name()).setHeader("negotiation", "testId").build(), TestStates.SUBMITTED.name()).subscribe();
-      assertEquals(count + 1, negotiationRepository.count());
-      log.info(negotiationRepository.count());
+    persistStateMachineHandler
+        .handleEventWithStateReactively(
+            MessageBuilder.withPayload(TestEvents.APPROVE.name())
+                .setHeader("negotiation", "testId")
+                .build(),
+            TestStates.SUBMITTED.name())
+        .subscribe();
+  }
+
+  @Test
+  void getPossibleEvents_ok() {
+    assertEquals(List.of(TestEvents.APPROVE), getPossibleEvents(TestStates.SUBMITTED));
+  }
+
+  private List<TestEvents> getPossibleEvents(TestStates testStates) {
+    entityStateMachine
+        .getStateMachineAccessor()
+        .doWithAllRegions(
+            accessor ->
+                accessor.resetStateMachineReactively(
+                    new DefaultStateMachineContext<>(testStates.name(), null, null, null)));
+    return entityStateMachine.getTransitions().stream()
+        .filter(
+            transition ->
+                transition.getSource().getId().equals(entityStateMachine.getState().getId()))
+        .map(transition -> transition.getTrigger().getEvent())
+        .map(TestEvents::valueOf)
+        .collect(Collectors.toList());
   }
 }
--- src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/TestStateMachineConfig.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/TestStateMachineConfig.java
@@ -1,31 +1,36 @@
 package eu.bbmri.eric.csit.service.negotiator.integration;
 
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
-
-import java.util.EnumSet;
-import java.util.HashSet;
-import java.util.Set;
 
 @Configuration
 @Profile("test")
 @EnableStateMachine(name = "testMachine")
 public class TestStateMachineConfig extends StateMachineConfigurerAdapter<String, String> {
+  @Override
+  public void configure(StateMachineConfigurationConfigurer<String, String> config)
+      throws Exception {
+    config.withConfiguration().autoStartup(true);
+  }
 
-    @Override
-    public void configure(StateMachineStateConfigurer<String, String> states) throws Exception {
-        Set<String> stringStates = new HashSet<>();
-        EnumSet.allOf(TestStates.class).forEach(entity -> stringStates.add(entity.name()));
-        states.withStates().initial(TestStates.SUBMITTED.name()).states(stringStates);
-    }
+  @Override
+  public void configure(StateMachineStateConfigurer<String, String> states) throws Exception {
+    Set<String> stringStates = new HashSet<>();
+    EnumSet.allOf(TestStates.class).forEach(entity -> stringStates.add(entity.name()));
+    states.withStates().initial(TestStates.SUBMITTED.name()).states(stringStates);
+  }
 
-    @Override
-    public void configure(StateMachineTransitionConfigurer<String, String> transitions)
-            throws Exception {
+  @Override
+  public void configure(StateMachineTransitionConfigurer<String, String> transitions)
+      throws Exception {
         transitions
                 .withExternal()
                 .source(TestStates.SUBMITTED.name())
