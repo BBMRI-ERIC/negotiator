f461bbe6e42fd7334ae12c264cb4f795cd7d9583
RadovanTomik <tomasik.radovan@gmail.com>
2023-07-23 15:44:48 +0200

    Refactor NegotiationLifecycleServiceImpl

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NegotiationLifecycleServiceImpl.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NegotiationLifecycleServiceImpl.java
@@ -1,6 +1,5 @@
 package eu.bbmri.eric.csit.service.negotiator.service;
 
-import eu.bbmri.eric.csit.service.negotiator.database.model.Negotiation;
 import eu.bbmri.eric.csit.service.negotiator.database.model.NegotiationEvent;
 import eu.bbmri.eric.csit.service.negotiator.database.model.NegotiationState;
 import eu.bbmri.eric.csit.service.negotiator.database.repository.NegotiationRepository;
@@ -37,56 +36,59 @@ public class NegotiationLifecycleServiceImpl implements NegotiationLifecycleServ
 
   @Override
   public NegotiationState getCurrentState(String negotiationId) throws EntityNotFoundException {
-    Negotiation negotiation =
-        negotiationRepository
-            .findById(negotiationId)
-            .orElseThrow(() -> new EntityNotFoundException("Negotiation not found."));
-    return negotiation.getCurrentState();
+   return getCurrentStateForNegotiation(negotiationId);
   }
 
   @Override
   public Set<NegotiationEvent> getPossibleEvents(String negotiationId)
       throws EntityNotFoundException {
-    Negotiation negotiation =
-        negotiationRepository
-            .findById(negotiationId)
-            .orElseThrow(() -> new EntityNotFoundException("Negotiation not found."));
-    stateMachine
-        .getStateMachineAccessor()
-        .doWithAllRegions(
-            accessor ->
-                accessor
-                    .resetStateMachineReactively(
-                        new DefaultStateMachineContext<>(
-                            negotiation.getCurrentState().name(), null, null, null))
-                    .subscribe());
-
-    return stateMachine.getTransitions().stream()
-        .filter(
-            transition -> transition.getSource().getId().equals(stateMachine.getState().getId()))
-        .map(transition -> transition.getTrigger().getEvent())
-        .map(NegotiationEvent::valueOf)
-        .collect(Collectors.toSet());
+    rehydrateStateMachineForNegotiation(negotiationId);
+    return getPossibleEventsForCurrentStateMachine();
   }
 
   @Override
   public NegotiationState sendEvent(String negotiationId, NegotiationEvent negotiationEvent)
       throws WrongRequestException, EntityNotFoundException {
-    NegotiationState currentState =
-        negotiationRepository
-            .findById(negotiationId)
-            .orElseThrow(() -> new EntityNotFoundException("Negotiation not found."))
-            .getCurrentState();
+    changeStateMachine(negotiationId, negotiationEvent);
+    return getCurrentStateForNegotiation(negotiationId);
+  }
+
+  private void changeStateMachine(String negotiationId, NegotiationEvent negotiationEvent) {
     persistStateMachineHandler
         .handleEventWithStateReactively(
             MessageBuilder.withPayload(negotiationEvent.name())
                 .setHeader("negotiationId", negotiationId)
                 .build(),
-            currentState.name())
+                getCurrentStateForNegotiation(negotiationId).name())
         .subscribe();
+  }
+
+  private NegotiationState getCurrentStateForNegotiation(String negotiationId) {
     return negotiationRepository
-        .findById(negotiationId)
-        .orElseThrow(() -> new EntityNotFoundException("Negotiation not found."))
-        .getCurrentState();
+            .findById(negotiationId)
+            .orElseThrow(() -> new EntityNotFoundException("Negotiation not found.")).getCurrentState();
+  }
+
+  private Set<NegotiationEvent> getPossibleEventsForCurrentStateMachine() {
+    return stateMachine.getTransitions().stream()
+            .filter(
+                    transition -> transition.getSource().getId().equals(stateMachine.getState().getId()))
+            .map(transition -> transition.getTrigger().getEvent())
+            .map(NegotiationEvent::valueOf)
+            .collect(Collectors.toSet());
+  }
+
+  private void rehydrateStateMachineForNegotiation(String negotiationId) {
+    NegotiationState currentState =
+            getCurrentStateForNegotiation(negotiationId);
+    stateMachine
+            .getStateMachineAccessor()
+            .doWithAllRegions(
+                    accessor ->
+                            accessor
+                                    .resetStateMachineReactively(
+                                            new DefaultStateMachineContext<>(
+                                                    currentState.name(), null, null, null))
+                                    .subscribe());
   }
 }
