bda08de658b5af11a8ccbe2022d2454e0586c591
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-07-24 15:13:18 +0200

    Remove NegotiationResourceLifecycleService implementation

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NegotiationResourceLifecycleServiceImpl.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NegotiationResourceLifecycleServiceImpl.java
@@ -5,84 +5,28 @@ import eu.bbmri.eric.csit.service.negotiator.database.model.NegotiationResourceS
 import eu.bbmri.eric.csit.service.negotiator.exceptions.EntityNotFoundException;
 import eu.bbmri.eric.csit.service.negotiator.exceptions.WrongRequestException;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.extern.apachecommons.CommonsLog;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.StateMachineEventResult;
-import org.springframework.statemachine.data.jpa.JpaStateMachineRepository;
-import org.springframework.statemachine.service.StateMachineService;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 
 @Service
 @CommonsLog
 public class NegotiationResourceLifecycleServiceImpl
     implements NegotiationResourceLifecycleService {
 
-  private static final String SEPARATOR = "---";
-  @Autowired JpaStateMachineRepository jpaStateMachineRepository;
-
-  @Autowired
-  @Qualifier("negotiationResourceStateMachineService")
-  private StateMachineService<NegotiationResourceState, NegotiationResourceEvent>
-      springNegotiationResourceStateMachineService;
-
   @Override
-  public void initializeTheStateMachine(String negotiationId, String resourceId) {
-    String resourceNegotiationId = createMachineId(negotiationId, resourceId);
-    springNegotiationResourceStateMachineService.acquireStateMachine(resourceNegotiationId, false);
-  }
+  public void initializeTheStateMachine(String negotiationId, String resourceId) {}
 
   @Override
   public NegotiationResourceState getCurrentState(String negotiationId, String resourceId)
-      throws EntityNotFoundException {
-    String negotiationResourceId = createMachineId(negotiationId, resourceId);
-    if (!jpaStateMachineRepository.existsById(negotiationResourceId)) {
-      throw new EntityNotFoundException("Combination of Negotiation and Resource does not exist");
-    }
-    return this.springNegotiationResourceStateMachineService
-        .acquireStateMachine(negotiationResourceId)
-        .getState()
-        .getId();
-  }
+      throws EntityNotFoundException {return null;}
 
   @Override
   public Set<NegotiationResourceEvent> getPossibleEvents(String negotiationId, String resourceId)
-      throws EntityNotFoundException {
-    String machineId = createMachineId(negotiationId, resourceId);
-    if (!jpaStateMachineRepository.existsById(machineId)) {
-      throw new EntityNotFoundException("StateMachine " + machineId + "does not exist.");
-    }
-    StateMachine<NegotiationResourceState, NegotiationResourceEvent> stateMachine =
-        this.springNegotiationResourceStateMachineService.acquireStateMachine(machineId);
-    return stateMachine.getTransitions().stream()
-        .filter(
-            transition -> transition.getSource().getId().equals(stateMachine.getState().getId()))
-        .map(transition -> transition.getTrigger().getEvent())
-        .collect(Collectors.toSet());
-  }
+      throws EntityNotFoundException {return null;}
 
   @Override
   public NegotiationResourceState sendEvent(
       String negotiationId, String resourceId, NegotiationResourceEvent negotiationEvent)
-      throws WrongRequestException, EntityNotFoundException {
-    StateMachine<NegotiationResourceState, NegotiationResourceEvent> stateMachine =
-        this.springNegotiationResourceStateMachineService.acquireStateMachine(
-            createMachineId(negotiationId, resourceId), true);
-    StateMachineEventResult<NegotiationResourceState, NegotiationResourceEvent> newState =
-        stateMachine
-            .sendEvent(Mono.just(MessageBuilder.withPayload(negotiationEvent).build()))
-            .blockFirst();
-    if (newState != null) {
-      log.info("Event was: " + newState.getResultType().toString());
-    }
-    return stateMachine.getState().getId();
-  }
-
-  private String createMachineId(String negotiationId, String resourceId) {
-    return negotiationId + SEPARATOR + resourceId;
-  }
+      throws WrongRequestException, EntityNotFoundException {return null;}
+  
 }
