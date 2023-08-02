3065b67c1b2d43a136f6bcfc4f06ff28144f8bbb
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-08-02 11:21:08 +0200

    Add REST endpoint for fetching lifecycle stages

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,33 +22,27 @@ values (1, 'BBMRI Template');
 insert into access_criteria_section (id, name, label, description, access_criteria_set_id)
 values (1, 'project', 'Project', 'Provide information about your project', 1);
 insert into access_criteria_section (id, name, label, description, access_criteria_set_id)
-values (2, 'samples', 'Biosamples and Data Information', 'Provide information about the biosamples you want', 1);
+values (2, 'request', 'Request', 'Provide information the resources you are requesting', 1);
 insert into access_criteria_section (id, name, label, description, access_criteria_set_id)
 values (3, 'ethics-vote', 'Ethics vote', 'Is ethics vote present in your project?', 1);
+insert into access_criteria_section (id, name, label, description, access_criteria_set_id)
+values (4, 'attachments', 'Attachments', 'Upload attachments:', 1);
 
 insert into access_criteria (id, name, label, description, type)
 values (1, 'title', 'Title', 'Give a title', 'text');
 insert into access_criteria (id, name, label, description, type)
 values (2, 'description', 'Description', 'Give a description', 'textarea');
 insert into access_criteria (id, name, label, description, type)
-values (3, 'num-of-subjects', 'Number of subjects', 'Number of biosamples', 'number');
+values (3, 'ethics-vote', 'Ethics vote', 'Write the etchics vote', 'text');
 insert into access_criteria (id, name, label, description, type)
-values (4, 'sample-type', 'Sample type(s)', 'Sample Type', 'text');
-insert into access_criteria (id, name, label, description, type)
-values (5, 'num-of-sample', 'Number of sample', 'Sample Type', 'text');
-insert into access_criteria (id, name, label, description, type)
-values (6, 'volume', 'Volume', 'Write the etchics vote', 'number');
-insert into access_criteria (id, name, label, description, type)
-values (7, 'ethics-vote', 'Ethics vote', 'Write the etchics vote', 'text');
+values (4, 'attachment', 'Attachment', 'Upload attachment', 'attachment');
 
 insert into access_criteria_section_link (access_criteria_section_id, access_criteria_id, ordering, required)
 values (1, 1, 1, 'true'),
        (1, 2, 2, 'false'),
-       (2, 3, 1, 'true'),
-       (2, 4, 2, 'false'),
-       (2, 5, 3, 'true'),
-       (2, 6, 4, 'false'),
-       (3, 7, 1, 'true');
+       (2, 2, 1, 'true'),
+       (3, 3, 1, 'true'),
+       (4,4,1, 'false');
 
 insert into resource (id, name, description, source_id, data_source_id, access_criteria_set_id)
 values (1, 'Test collection', 'This is the fist test collection',
