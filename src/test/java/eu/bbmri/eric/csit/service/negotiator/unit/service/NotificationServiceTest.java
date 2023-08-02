b141f5ff67b39544d3ca4d2126fd7f55dd9407c8
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-08-02 12:02:43 +0200

    Add basic interface of the NotificationService

---
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/service/NotificationServiceTest.java
@@ -0,0 +1,19 @@
+package eu.bbmri.eric.csit.service.negotiator.unit.service;
+
+import eu.bbmri.eric.csit.service.negotiator.service.NotificationService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NotificationServiceTest {
+    private final NotificationService notificationService = new NotificationService() {
+        @Override
+        public boolean sendEmailToResourceRepresentatives(String resourceId, String mailBody) {
+            return true;
+        }
+    };
+  @Test
+  void sendEmailToResourceRepresentatives_emptyContent_Ok() {
+      assertTrue(notificationService.sendEmailToResourceRepresentatives("collection:1", ""));
+  }
+}
