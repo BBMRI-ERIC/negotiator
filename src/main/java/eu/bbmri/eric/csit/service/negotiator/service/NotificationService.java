9730dd9fa00f4885cde1e36fa5730ca368dbe2c9
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-08-02 16:10:54 +0200

    Remove notification service impl

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NotificationService.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NotificationService.java
@@ -3,13 +3,4 @@ package eu.bbmri.eric.csit.service.negotiator.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface NotificationService {
-  /**
-   * Sends an email notification to Resource Representatives.
-   *
-   * @param resourceId of which representatives you want to contact.
-   * @param mailBody content of the email message.
-   * @return true if email was successfully sent out.
-   */
-  boolean sendEmailToResourceRepresentatives(String resourceId, String mailBody);
-}
+public interface NotificationService {}
--- src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/NotificationServiceTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/NotificationServiceTest.java
@@ -1,24 +1,10 @@
 package eu.bbmri.eric.csit.service.negotiator.integration;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import eu.bbmri.eric.csit.service.negotiator.NegotiatorApplication;
-import eu.bbmri.eric.csit.service.negotiator.service.NotificationService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = NegotiatorApplication.class)
 @ActiveProfiles("test")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class NotificationServiceTest {
-
-  @Autowired private NotificationService notificationService;
-
-  @Test
-  void sendEmailToRepresentatives_wrongSMTPConfig_Ok() {
-    assertFalse(notificationService.sendEmailToResourceRepresentatives("", ""));
-  }
-}
+public class NotificationServiceTest {}
