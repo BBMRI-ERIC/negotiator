546e517e66a2a344dce822ff7a7d734a109a678d
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-08-02 16:28:51 +0200

    Add email validation

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NotificationServiceImpl.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/service/NotificationServiceImpl.java
@@ -3,10 +3,21 @@ package eu.bbmri.eric.csit.service.negotiator.service;
 
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 @NoArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
     @Override
     public boolean sendEmail(String recipientAddress) {
-        return false;
+        return validateEmailAddress(recipientAddress);
+    }
+
+    private static boolean validateEmailAddress(String recipientAddress) {
+        if (Objects.isNull(recipientAddress)){
+            return false;
+        }
+        String regexPattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        return Pattern.compile(regexPattern).matcher(recipientAddress).matches();
     }
 }
--- src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/NotificationServiceTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/NotificationServiceTest.java
@@ -5,10 +5,23 @@ import eu.bbmri.eric.csit.service.negotiator.service.NotificationService;
 import eu.bbmri.eric.csit.service.negotiator.service.NotificationServiceImpl;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class NotificationServiceTest {
   private final NotificationService notificationService = new NotificationServiceImpl();
   @Test
   void sendEmail_nullRecipient_returnsFalse() {
+    assertFalse(notificationService.sendEmail(null));
+  }
 
+  @Test
+  void sendEmail_recipientIsRandomString_returnsFalse() {
+    assertFalse(notificationService.sendEmail("random"));
+  }
+
+  @Test
+  void sendEmail_validRecipientEmail_returns() {
+    assertTrue(notificationService.sendEmail("test@test.com"));
   }
 }
