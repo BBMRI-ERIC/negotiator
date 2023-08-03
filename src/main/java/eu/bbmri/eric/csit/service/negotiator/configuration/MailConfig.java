1e29563d2553de2ae9a68fcb9be1d729a3a5fc8e
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-08-03 10:20:30 +0200

    Refactor NotificationService

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/MailConfig.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/MailConfig.java
@@ -14,11 +14,19 @@ public class MailConfig {
   @Value("${spring.mail.port}")
   private int port;
 
+  @Value("${spring.mail.username}")
+  private String username;
+
+  @Value("${spring.mail.password}")
+  private String password;
+
   @Bean
   public JavaMailSender getJavaMailSender() {
     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
     mailSender.setHost(host);
     mailSender.setPort(port);
+    mailSender.setUsername(username);
+    mailSender.setPassword(password);
     return mailSender;
   }
 }
