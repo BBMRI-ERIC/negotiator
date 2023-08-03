31993a52fa4153ec05351c6ca3a1f7c11f822681
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-08-03 11:01:44 +0200

    Fix email configuration

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/MailConfig.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/configuration/MailConfig.java
@@ -1,5 +1,6 @@
 package eu.bbmri.eric.csit.service.negotiator.configuration;
 
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +24,11 @@ public class MailConfig {
   @Bean
   public JavaMailSender getJavaMailSender() {
     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
     mailSender.setHost(host);
     mailSender.setPort(port);
     mailSender.setUsername(username);
