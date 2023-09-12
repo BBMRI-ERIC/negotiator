88227e39dc95484259187f94f1d9b33ae2389d0a
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-09-12 17:58:47 +0200

    feat: add OrganizationDTO
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

---
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/dto/OrganizationDTO.java
@@ -0,0 +1,22 @@
+package eu.bbmri.eric.csit.service.negotiator.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+public class OrganizationDTO {
+  @NonNull private String externalId;
+  private String name;
+}
