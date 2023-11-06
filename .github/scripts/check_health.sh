70e58400d76783087406a44b99e40a907dfbae75
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-11-06 16:33:10 +0100

    ci: add smoke test
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

---
+++ .github/scripts/check_health.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CONTAINER_NAME_OR_ID=$1
+MAX_WAIT_TIME=60
+
+
+ELAPSED_TIME=0
+HEALTH_STATUS=""
+
+while [ "$ELAPSED_TIME" -lt "$MAX_WAIT_TIME" ]; do
+  HEALTH_STATUS=$(docker inspect --format='{{.State.Health.Status}}' "$CONTAINER_NAME_OR_ID")
+
+  if [ "$HEALTH_STATUS" = "healthy" ]; then
+    echo "Container is healthy"
+    exit 0
+  else
+    echo "Container is not healthy. Waiting for it to become healthy..."
+    sleep 10
+    ELAPSED_TIME=$((ELAPSED_TIME + 10))
+  fi
+done
+
+echo "Container did not become healthy within $MAX_WAIT_TIME seconds."
+exit 1
