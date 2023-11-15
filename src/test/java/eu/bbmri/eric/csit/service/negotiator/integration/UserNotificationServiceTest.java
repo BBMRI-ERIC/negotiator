fdc2523bbb4c3161e23b3a9028794f61b9fbc7d0
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-11-15 16:50:29 +0100

    tests: fix db cleaning in UserNotificationServiceTest
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/UserNotificationServiceTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/UserNotificationServiceTest.java
@@ -21,10 +21,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserNotificationServiceTest {
 
   @Autowired UserNotificationService userNotificationService;
