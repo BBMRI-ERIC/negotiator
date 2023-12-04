40b50532fbc8228d775c6ffad750783bd179b5bd
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2023-12-04 16:26:29 +0100

    test: add Pagination and sorting tests for PersonRepository
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/repository/PersonRepositoryTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/repository/PersonRepositoryTest.java
@@ -1,5 +1,6 @@
 package eu.bbmri.eric.csit.service.negotiator.integration.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import eu.bbmri.eric.csit.service.negotiator.database.model.DataSource;
@@ -14,6 +15,9 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
@@ -47,9 +51,7 @@ public class PersonRepositoryTest {
                 .name("")
                 .syncActive(true)
                 .build());
-    Person person =
-        personRepository.save(
-            Person.builder().subjectId("823").name("John").email("test@test.com").build());
+    Person person = savePerson("test");
     Resource resource =
         resourceRepository.save(
             Resource.builder()
@@ -61,5 +63,49 @@ public class PersonRepositoryTest {
                 .build());
     assertTrue(personRepository.existsById(person.getId()));
     assertTrue(personRepository.existsByIdAndResourcesIn(person.getId(), Set.of(resource)));
+  }
+
+  @Test
+  void findAllPage_onePerson_ok() {
+    savePerson("test");
+    assertEquals(1L, personRepository.findAll(PageRequest.of(0, 1)).getTotalElements());
+    assertEquals(1, personRepository.findAll(PageRequest.of(0, 1)).getSize());
+  }
+
+  @Test
+  void findAllPage_1000People_ok() {
+    for (int i = 0; i < 1000; i++) {
+      savePerson("test-" + i);
+    }
+    assertEquals(1000L, personRepository.findAll(PageRequest.of(0, 1)).getTotalElements());
+    assertEquals(50, personRepository.findAll(PageRequest.of(0, 50)).getSize());
+  }
+
+  @Test
+  void findAllPageWithSort_1001People_nameWithAIsFirst() {
+    for (int i = 0; i < 1000; i++) {
+      savePerson("test-" + i);
+    }
+    Person person = savePerson("a-test", "AAAA");
+    assertEquals(
+        person.getSubjectId(),
+        personRepository
+            .findAll(PageRequest.of(0, 50, Sort.by("name")))
+            .getContent()
+            .get(0)
+            .getSubjectId());
+    Page<Person> pageDescending =
+        personRepository.findAll(PageRequest.of(20, 50, Sort.by("name").descending()));
+    assertEquals(person.getSubjectId(), pageDescending.getContent().get(0).getSubjectId());
+  }
+
+  private Person savePerson(String subjectId) {
+    return personRepository.save(
+        Person.builder().subjectId(subjectId).name("John").email("test@test.com").build());
+  }
+
+  private Person savePerson(String subjectId, String name) {
+    return personRepository.save(
+        Person.builder().subjectId(subjectId).name(name).email("test@test.com").build());
   }
 }
