0d67b17e81672fadd9316fe5de9d80f89ab9b0f2
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-01-17 08:48:00 +0100

    chore: code style reformatting
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/repository/NegotiationRepositoryTest.java
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/integration/repository/NegotiationRepositoryTest.java
@@ -24,75 +24,85 @@ import org.springframework.test.context.TestPropertySource;
 @DataJpaTest
 @TestPropertySource(properties = {"spring.sql.init.mode=never"})
 public class NegotiationRepositoryTest {
-    @Autowired
-    PersonRepository personRepository;
+  @Autowired PersonRepository personRepository;
 
-    @Autowired
-    ResourceRepository resourceRepository;
+  @Autowired ResourceRepository resourceRepository;
 
-    @Autowired
-    RequestRepository requestRepository;
+  @Autowired RequestRepository requestRepository;
 
-    @Autowired
-    DataSourceRepository dataSourceRepository;
+  @Autowired DataSourceRepository dataSourceRepository;
 
-    @Autowired
-    OrganizationRepository organizationRepository;
-    @Autowired
-    NegotiationRepository negotiationRepository;
+  @Autowired OrganizationRepository organizationRepository;
+  @Autowired NegotiationRepository negotiationRepository;
 
-    String payload =
-            "    {\n"
-                    + "\"project\": {\n"
-                    + "\"title\": \"Title\",\n"
-                    + "\"description\": \"Description\"\n"
-                    + "},\n"
-                    + " \"samples\": {\n"
-                    + "   \"sample-type\": \"DNA\",\n"
-                    + "   \"num-of-subjects\": 10,\n"
-                    + "   \"num-of-samples\": 20,\n"
-                    + "   \"volume-per-sample\": 5\n"
-                    + " },\n"
-                    + " \"ethics-vote\": {\n"
-                    + "   \"ethics-vote\": \"My ethic vote\"\n"
-                    + " }\n"
-                    + "}\n";
+  String payload =
+      "    {\n"
+          + "\"project\": {\n"
+          + "\"title\": \"Title\",\n"
+          + "\"description\": \"Description\"\n"
+          + "},\n"
+          + " \"samples\": {\n"
+          + "   \"sample-type\": \"DNA\",\n"
+          + "   \"num-of-subjects\": 10,\n"
+          + "   \"num-of-samples\": 20,\n"
+          + "   \"volume-per-sample\": 5\n"
+          + " },\n"
+          + " \"ethics-vote\": {\n"
+          + "   \"ethics-vote\": \"My ethic vote\"\n"
+          + " }\n"
+          + "}\n";
 
   @Test
   void save_ok() {
-      Organization organization =
-              organizationRepository.save(
-                      Organization.builder().name("test").externalId("biobank:1").build());
-      DataSource dataSource =
-              dataSourceRepository.save(
-                      DataSource.builder()
-                              .sourcePrefix("")
-                              .apiPassword("")
-                              .apiType(DataSource.ApiType.MOLGENIS)
-                              .apiUrl("")
-                              .apiUsername("")
-                              .url("")
-                              .resourceBiobank("")
-                              .resourceCollection("")
-                              .resourceNetwork("")
-                              .name("")
-                              .syncActive(true)
-                              .build());
-      Person person = savePerson("test");
-      Resource resource = resourceRepository.save(
-                      Resource.builder()
-                              .organization(organization)
-                              .dataSource(dataSource)
-                              .sourceId("collection:1")
-                              .name("test")
-                              .representatives(Set.of(person))
-                              .build());
-      Request request = Request.builder().id("1").url("http://test").resources(Set.of(resource)).dataSource(dataSource).humanReadable("everything").build();
-      Negotiation negotiation = Negotiation.builder().currentState(NegotiationState.IN_PROGRESS).requests(Set.of(request)).postsEnabled(false).payload(payload).build();
-      assertEquals("1", negotiationRepository.save(negotiation).getRequests().iterator().next().getId());
+    Organization organization =
+        organizationRepository.save(
+            Organization.builder().name("test").externalId("biobank:1").build());
+    DataSource dataSource =
+        dataSourceRepository.save(
+            DataSource.builder()
+                .sourcePrefix("")
+                .apiPassword("")
+                .apiType(DataSource.ApiType.MOLGENIS)
+                .apiUrl("")
+                .apiUsername("")
+                .url("")
+                .resourceBiobank("")
+                .resourceCollection("")
+                .resourceNetwork("")
+                .name("")
+                .syncActive(true)
+                .build());
+    Person person = savePerson("test");
+    Resource resource =
+        resourceRepository.save(
+            Resource.builder()
+                .organization(organization)
+                .dataSource(dataSource)
+                .sourceId("collection:1")
+                .name("test")
+                .representatives(Set.of(person))
+                .build());
+    Request request =
+        Request.builder()
+            .id("1")
+            .url("http://test")
+            .resources(Set.of(resource))
+            .dataSource(dataSource)
+            .humanReadable("everything")
+            .build();
+    Negotiation negotiation =
+        Negotiation.builder()
+            .currentState(NegotiationState.IN_PROGRESS)
+            .requests(Set.of(request))
+            .postsEnabled(false)
+            .payload(payload)
+            .build();
+    assertEquals(
+        "1", negotiationRepository.save(negotiation).getRequests().iterator().next().getId());
   }
-    private Person savePerson(String subjectId) {
-        return personRepository.save(
-                Person.builder().subjectId(subjectId).name("John").email("test@test.com").build());
-    }
+
+  private Person savePerson(String subjectId) {
+    return personRepository.save(
+        Person.builder().subjectId(subjectId).name("John").email("test@test.com").build());
+  }
 }
