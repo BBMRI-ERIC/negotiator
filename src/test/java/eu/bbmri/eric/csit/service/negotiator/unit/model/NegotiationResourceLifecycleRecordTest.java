e4bbd3fc80fca0dd8e278428905bab9f3ea2b27b
asulis <alessandro.sulis@crs4.it>
2024-01-22 09:29:50 +0100

    test: Adds test for resource lifecycle record entity creation

---
+++ src/test/java/eu/bbmri/eric/csit/service/negotiator/unit/model/NegotiationResourceLifecycleRecordTest.java
@@ -0,0 +1,27 @@
+package eu.bbmri.eric.csit.service.negotiator.unit.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import eu.bbmri.eric.csit.service.negotiator.configuration.state_machine.resource.NegotiationResourceState;
+import eu.bbmri.eric.csit.service.negotiator.database.model.Negotiation;
+import eu.bbmri.eric.csit.service.negotiator.database.model.NegotiationResourceLifecycleRecord;
+import eu.bbmri.eric.csit.service.negotiator.database.model.Resource;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+
+public class NegotiationResourceLifecycleRecordTest {
+
+  @Test
+  void buildAllParametersOK() {
+    NegotiationResourceLifecycleRecord negotiationResourceLifecycleRecord =
+        NegotiationResourceLifecycleRecord.builder()
+            .negotiation(new Negotiation())
+            .resource(new Resource())
+            .changedTo(NegotiationResourceState.RESOURCE_UNAVAILABLE)
+            .recordedAt(LocalDateTime.now())
+            .build();
+    assertEquals(
+        NegotiationResourceState.RESOURCE_UNAVAILABLE,
+        negotiationResourceLifecycleRecord.getChangedTo());
+  }
+}
