f2053ff38016eff1fd7435c6c4c0a1581e709504
asulis <alessandro.sulis@crs4.it>
2024-01-22 10:03:34 +0100

    feat: Adds person information to the entity

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/database/model/NegotiationResourceLifecycleRecord.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/database/model/NegotiationResourceLifecycleRecord.java
@@ -2,13 +2,9 @@ package eu.bbmri.eric.csit.service.negotiator.database.model;
 
 import eu.bbmri.eric.csit.service.negotiator.configuration.state_machine.resource.NegotiationResourceState;
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
@@ -22,12 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public final class NegotiationResourceLifecycleRecord {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
-  private Long id;
+public final class NegotiationResourceLifecycleRecord extends AuditEntity {
 
   @ManyToOne(cascade = CascadeType.ALL)
   @JoinColumn(name = "negotiation_id", referencedColumnName = "id")
