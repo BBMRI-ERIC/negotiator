e4feda44ac3938031aa422d6251696f0a149fdf1
asulis <alessandro.sulis@crs4.it>
2024-01-24 09:33:44 +0100

    feat: Adds audit extension to include Person

--- src/main/java/eu/bbmri/eric/csit/service/negotiator/database/model/NegotiationResourceLifecycleRecord.java
+++ src/main/java/eu/bbmri/eric/csit/service/negotiator/database/model/NegotiationResourceLifecycleRecord.java
@@ -2,13 +2,9 @@ package eu.bbmri.eric.csit.service.negotiator.database.model;
 
 import eu.bbmri.eric.csit.service.negotiator.configuration.state_machine.resource.NegotiationResourceState;
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.ZonedDateTime;
@@ -22,11 +18,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class NegotiationResourceLifecycleRecord {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
-  private Long id;
+public class NegotiationResourceLifecycleRecord extends AuditEntity {
+  // @Id
+  // @GeneratedValue(strategy = GenerationType.IDENTITY)
+  // @Column(name = "id")
+  // private Long id;
 
   @ManyToOne(cascade = CascadeType.ALL)
   @JoinColumn(name = "resource_id", referencedColumnName = "id")
