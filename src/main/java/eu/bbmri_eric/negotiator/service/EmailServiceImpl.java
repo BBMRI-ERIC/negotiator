16ee5a9f2494c16db93e9436a9bcc6ecd95e90b9
Emilian Jungwirth <emilian.jung+github@gmail.com>
2024-02-20 17:47:31 +0100

    fix: format

--- src/main/java/eu/bbmri_eric/negotiator/service/EmailServiceImpl.java
+++ src/main/java/eu/bbmri_eric/negotiator/service/EmailServiceImpl.java
@@ -4,9 +4,7 @@ import eu.bbmri_eric.negotiator.database.model.NotificationEmail;
 import eu.bbmri_eric.negotiator.database.model.Person;
 import eu.bbmri_eric.negotiator.database.repository.NotificationEmailRepository;
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
-
 import java.io.UnsupportedEncodingException;
 import java.util.Objects;
 import java.util.regex.Pattern;
@@ -53,13 +51,13 @@ public class EmailServiceImpl implements EmailService {
       helper.setText(mailBody, true);
       helper.setTo(recipientAddress);
       helper.setSubject(subject);
-      helper.setFrom("noreply@bbmri-eric.eu","BBMRI-ERIC negotiator" );
+      helper.setFrom("noreply@bbmri-eric.eu", "BBMRI-ERIC negotiator");
     } catch (MessagingException e) {
       throw new NullPointerException(e.toString());
     } catch (UnsupportedEncodingException e) {
-        throw new RuntimeException(e);
+      throw new RuntimeException(e);
     }
-      return mimeMessage;
+    return mimeMessage;
   }
 
   private static boolean isValidEmailAddress(String recipientAddress) {
@@ -83,7 +81,7 @@ public class EmailServiceImpl implements EmailService {
     } catch (NullPointerException e) {
       log.error("Failed to send email. Check message content.");
       return;
-    }catch (RuntimeException e) {
+    } catch (RuntimeException e) {
       log.error("Failed to send email.");
       return;
     }
