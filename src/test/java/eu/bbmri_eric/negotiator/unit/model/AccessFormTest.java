c9b8d51982ab6582885750afde18f97710a33123
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-02-29 11:21:19 +0100

    test: access form proper linkage
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/test/java/eu/bbmri_eric/negotiator/unit/model/AccessFormTest.java
+++ src/test/java/eu/bbmri_eric/negotiator/unit/model/AccessFormTest.java
@@ -1,6 +1,10 @@
 package eu.bbmri_eric.negotiator.unit.model;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import eu.bbmri_eric.negotiator.database.model.AccessForm;
+import eu.bbmri_eric.negotiator.database.model.AccessFormElement;
+import eu.bbmri_eric.negotiator.database.model.AccessFormSection;
 import org.junit.jupiter.api.Test;
 
 public class AccessFormTest {
@@ -9,5 +13,25 @@ public class AccessFormTest {
   void initAccessForm_noArgsConstructor_ok() {
     new AccessForm("test");
   }
-  
+
+  @Test
+  void linkElementToSection_sectionNotInForm_throwsIllegalArg() {
+    AccessForm accessForm = new AccessForm("test");
+    assertThrows(IllegalArgumentException.class, () -> accessForm.linkElementToSection(new AccessFormSection("test", "test", "test"),
+            new AccessFormElement("test", "test", "test", "test"), 0, true));
+  }
+
+  @Test
+  void linkElementToSection_sectionNotLinkedToElement_throwsIllegalArg() {
+    AccessForm accessForm = new AccessForm("test");
+    AccessFormSection notAllowedSection = new AccessFormSection("test", "test", "test");
+    notAllowedSection.setId(1L);
+    AccessFormSection allowedSection = new AccessFormSection("test", "test", "test");
+    allowedSection.setId(2L);
+    AccessFormElement element = new AccessFormElement("test", "test", "test", "test");
+    element.setLinkedSection(allowedSection);
+    accessForm.linkSection(notAllowedSection, 0);
+    assertThrows(IllegalArgumentException.class, () -> accessForm.linkElementToSection(notAllowedSection,
+            element, 0, true));
+  }
 }
