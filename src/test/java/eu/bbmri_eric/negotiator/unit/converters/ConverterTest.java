2d9022bab2015434c75a1c02fc462590400a1809
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-03-08 16:52:58 +0100

    tests: add tests for converters
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

---
+++ src/test/java/eu/bbmri_eric/negotiator/unit/converters/ConverterTest.java
@@ -0,0 +1,34 @@
+package eu.bbmri_eric.negotiator.unit.converters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import eu.bbmri_eric.negotiator.converters.NegotiationRoleConverter;
+import eu.bbmri_eric.negotiator.converters.NegotiationStatusConverter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+
+public class ConverterTest {
+
+    @Test
+  public void testConvert() {
+    NegotiationRoleConverter converter =
+        new NegotiationRoleConverter();
+    assertThrows(
+            ResponseStatusException.class,
+        () -> {
+          converter.convert("test");
+        });
+  }
+
+  @Test
+  void testStatusConverter() {
+      NegotiationStatusConverter converter = new NegotiationStatusConverter();
+        assertThrows(
+                ResponseStatusException.class,
+                () -> {
+                    converter.convert("test");
+                });
+    assertEquals("APPROVED", converter.convert("APPROVED").toString());
+  }
+}
