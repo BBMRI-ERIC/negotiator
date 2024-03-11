fd3d265f0d41c772a919af5b68466969c016235b
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-03-11 10:43:08 +0100

    feat(access-form): add combination of required elements
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/test/java/eu/bbmri_eric/negotiator/unit/mappers/AccessFormElementMapperTest.java
+++ src/test/java/eu/bbmri_eric/negotiator/unit/mappers/AccessFormElementMapperTest.java
@@ -2,15 +2,12 @@ package eu.bbmri_eric.negotiator.unit.mappers;
 
 import eu.bbmri_eric.negotiator.mappers.AccessFormModelsMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.modelmapper.ModelMapper;
 
 public class AccessFormElementMapperTest {
-  @Spy public ModelMapper mapper = new ModelMapper();
-
-  @InjectMocks
+  public ModelMapper mapper = new ModelMapper();
   public AccessFormModelsMapper accessFormModelsMapper = new AccessFormModelsMapper(mapper);
 
   @BeforeEach
@@ -19,41 +16,6 @@ public class AccessFormElementMapperTest {
     this.accessFormModelsMapper.addMappings();
   }
 
-  //  @Test
-  //  void map_name_Ok() {
-  //    SortedSet<AccessFormSection> sections = new TreeSet<>();
-  //    AccessFormSection accessCriteriaSection = new AccessFormSection();
-  //    AccessCriteriaSectionLink accessCriteriaSectionLink = new AccessCriteriaSectionLink();
-  //    AccessFormElement accessCriteria = new AccessFormElement();
-  //    accessCriteria.setName("test criteria");
-  //    accessCriteria.setLabel("test label");
-  //    accessCriteria.setDescription("test desc");
-  //    accessCriteria.setType("test type");
-  //    accessCriteriaSectionLink.setAccessFormElement(accessCriteria);
-  //    accessCriteriaSectionLink.setRequired(true);
-  //    //accessCriteriaSection.setAccessCriteriaSectionLink(List.of(accessCriteriaSectionLink));
-  //    accessCriteriaSection.setId(1L);
-  //    accessCriteriaSection.setName("test section");
-  //    accessCriteriaSection.setLabel("test section label");
-  //    accessCriteriaSection.setDescription("test section desc");
-  //    sections.add(accessCriteriaSection);
-  //    AccessForm accessForm =
-  //        new AccessForm(1L, "test", Set.of(new Resource()), sections);
-  //    AccessFormDTO accessCriteriaSetDTO =
-  //        mapper.map(accessForm, AccessFormDTO.class);
-  //    assertEquals(accessForm.getName(), accessCriteriaSetDTO.getName());
-  //    assertEquals(
-  //        accessCriteriaSection.getName(),
-  //        accessCriteriaSetDTO.getSections().iterator().next().getName());
-  //    assertEquals(
-  //        accessCriteria.getName(),
-  //        accessCriteriaSetDTO
-  //            .getSections()
-  //            .iterator()
-  //            .next()
-  //            .getAccessFormElement()
-  //            .iterator()
-  //            .next()
-  //            .getName());
-  //  }
+  @Test
+  void name() {}
 }
