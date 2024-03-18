3892e42d56ac7934765b35a793ae95e59fb2344c
Emilian Jungwirth <emilian.jung+github@gmail.com>
2024-03-18 10:09:44 +0100

    refactor: typo

--- src/main/java/eu/bbmri_eric/negotiator/configuration/state_machine/negotiation/DisablePostsAction.java
+++ src/main/java/eu/bbmri_eric/negotiator/configuration/state_machine/negotiation/DisablePostsAction.java
@@ -7,7 +7,7 @@ import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
 
-/** SpringStateMachine action for enabling posts in a Negotiation */
+/** SpringStateMachine action for disabling posts in a Negotiation */
 @Component
 public class DisablePostsAction implements Action<String, String> {
 
