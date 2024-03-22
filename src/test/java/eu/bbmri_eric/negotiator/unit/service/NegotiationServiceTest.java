409020ec93e2a41250f8a93ad95c22792dc66c14
Vittorio Meloni <vittorio.meloni@crs4.it>
2024-03-22 11:43:12 +0100

    fix: fixes tests after rebase

--- src/test/java/eu/bbmri_eric/negotiator/unit/service/NegotiationServiceTest.java
+++ src/test/java/eu/bbmri_eric/negotiator/unit/service/NegotiationServiceTest.java
@@ -2,6 +2,7 @@ package eu.bbmri_eric.negotiator.unit.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
