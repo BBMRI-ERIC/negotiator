858b9578e4e9082c9196153e9a50005aa2c6e2a0
Emilian Jungwirth <emilian.jung+github@gmail.com>
2024-04-15 11:58:57 +0200

    test: update build NegotiationResourceLifecycleRecord test

--- src/test/java/eu/bbmri_eric/negotiator/unit/model/NegotiationResourceLifecycleRecordTest.java
+++ src/test/java/eu/bbmri_eric/negotiator/unit/model/NegotiationResourceLifecycleRecordTest.java
@@ -1,8 +1,9 @@
 package eu.bbmri_eric.negotiator.unit.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import eu.bbmri_eric.negotiator.configuration.state_machine.resource.NegotiationResourceState;
+import eu.bbmri_eric.negotiator.database.model.Negotiation;
 import eu.bbmri_eric.negotiator.database.model.NegotiationResourceLifecycleRecord;
 import eu.bbmri_eric.negotiator.database.model.Resource;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,17 @@ public class NegotiationResourceLifecycleRecordTest {
 
   @Test
   void buildAllParametersOK() {
+    Resource resource = new Resource();
+    Negotiation negotiation = new Negotiation();
+
     NegotiationResourceLifecycleRecord negotiationResourceLifecycleRecord =
         NegotiationResourceLifecycleRecord.builder()
-            .resource(new Resource())
+            .resource(resource)
+            .negotiation(negotiation)
             .changedTo(NegotiationResourceState.RESOURCE_UNAVAILABLE)
             .build();
+    assertSame(negotiation, negotiationResourceLifecycleRecord.getNegotiation());
+    assertSame(resource, negotiationResourceLifecycleRecord.getResource());
     assertEquals(
         NegotiationResourceState.RESOURCE_UNAVAILABLE,
         negotiationResourceLifecycleRecord.getChangedTo());
