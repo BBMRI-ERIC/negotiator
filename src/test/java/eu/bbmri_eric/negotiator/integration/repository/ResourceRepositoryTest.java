8a2076c574ad6ff3a598c8037ed1e565103ce64b
RadovanTomik <tomasik.radovan@gmail.com>
2024-04-17 15:28:24 +0200

    fix(tests): add db cleaning after each test

--- src/test/java/eu/bbmri_eric/negotiator/integration/repository/ResourceRepositoryTest.java
+++ src/test/java/eu/bbmri_eric/negotiator/integration/repository/ResourceRepositoryTest.java
@@ -13,12 +13,14 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest(showSql = false)
 @ActiveProfiles("test")
 @TestPropertySource(properties = {"spring.sql.init.mode=never"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ResourceRepositoryTest {
 
   @Autowired PersonRepository personRepository;
