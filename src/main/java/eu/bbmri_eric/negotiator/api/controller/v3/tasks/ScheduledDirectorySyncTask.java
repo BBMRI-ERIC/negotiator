8b952b382c905060e1e6de44b41747871db7d536
asulis <alessandro.sulis@crs4.it>
2024-05-20 14:39:32 +0200

    feat: Sets scheduled task to run every day at midnight

--- src/main/java/eu/bbmri_eric/negotiator/api/controller/v3/tasks/ScheduledDirectorySyncTask.java
+++ src/main/java/eu/bbmri_eric/negotiator/api/controller/v3/tasks/ScheduledDirectorySyncTask.java
@@ -3,6 +3,7 @@ package eu.bbmri_eric.negotiator.api.controller.v3.tasks;
 import eu.bbmri_eric.negotiator.service.MolgenisDirectorySyncService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,7 @@ public class ScheduledDirectorySyncTask {
 
   @Autowired private MolgenisDirectorySyncService molgenisDirectorySyncService;
 
-  // @Scheduled(cron = "0 * * * * *") // Cron expression for running every minute
+  @Scheduled(cron = "0 0 0 * * ?") // Cron expression for running every day at midnight
   @Async
   public void syncMolgenisResources() {
     molgenisDirectorySyncService.syncDirectoryResources();
