236cd5ab7a08f221dc12d06d0015f9219ae079ba
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-05-21 14:50:12 +0200

    fix(notifications): make representative notifications generation async
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/main/java/eu/bbmri_eric/negotiator/configuration/state_machine/resource/ResourcePersistStateChangeListener.java
+++ src/main/java/eu/bbmri_eric/negotiator/configuration/state_machine/resource/ResourcePersistStateChangeListener.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.lang.Nullable;
 import org.springframework.messaging.Message;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.recipes.persist.PersistStateMachineHandler;
 import org.springframework.statemachine.state.State;
@@ -56,8 +55,7 @@ public class ResourcePersistStateChangeListener
     return message.getHeaders().get("resourceId", String.class);
   }
 
-  @Async
-  protected void notifyRequester(Negotiation negotiation, String resourceId) {
+  private void notifyRequester(Negotiation negotiation, String resourceId) {
     userNotificationService.notifyRequesterAboutStatusChange(
         negotiation,
         negotiation.getResources().stream()
