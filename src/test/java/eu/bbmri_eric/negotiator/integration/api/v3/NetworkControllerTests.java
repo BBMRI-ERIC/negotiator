bac66c645a79764f3b350df82fbad67021e19913
Emilian Jungwirth <emilian.jung+github@gmail.com>
2024-05-24 10:03:25 +0200

    tests: adds integration tests for delete, create, update network and add/remove managers and resources

--- src/test/java/eu/bbmri_eric/negotiator/integration/api/v3/NetworkControllerTests.java
+++ src/test/java/eu/bbmri_eric/negotiator/integration/api/v3/NetworkControllerTests.java
@@ -1,20 +1,32 @@
 package eu.bbmri_eric.negotiator.integration.api.v3;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.jayway.jsonpath.JsonPath;
 import eu.bbmri_eric.negotiator.NegotiatorApplication;
+import eu.bbmri_eric.negotiator.database.model.Network;
+import eu.bbmri_eric.negotiator.database.repository.NetworkRepository;
+import eu.bbmri_eric.negotiator.dto.NetworkDTO;
+import java.util.Optional;
+
+import jakarta.transaction.Transactional;
 import lombok.extern.apachecommons.CommonsLog;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -30,6 +42,7 @@ public class NetworkControllerTests {
   @Autowired private WebApplicationContext context;
 
   private MockMvc mockMvc;
+  @Autowired private NetworkRepository networkRepository;
 
   @BeforeEach
   public void before() {
@@ -95,5 +108,133 @@ public class NetworkControllerTests {
         .andExpect(jsonPath("$._embedded.negotiations.[1].id", is("negotiation-5")))
         .andExpect(jsonPath("$._embedded.negotiations.[2].id", is("negotiation-3")))
         .andExpect(jsonPath("$._embedded.negotiations.[3].id", is("negotiation-4")));
+  }
+
+  @Test
+  public void testDelete_Network_ok() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.delete(NETWORKS_URL + "/1"))
+        .andExpect(status().isNoContent());
+
+    Optional<Network> network = networkRepository.findById(1L);
+    assertFalse(network.isPresent());
+  }
+
+  @Test
+  public void testDelete_Network_EntityNotFound() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.delete(NETWORKS_URL + "/314"))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void testCreate_Network_ok() throws Exception {
+    NetworkDTO networkDTO =
+        NetworkDTO.builder()
+            .externalId("externalId")
+            .contactEmail("new@negotiator.com")
+            .name("newNetwork")
+            .uri("http://newuri.org")
+            .build();
+    String requestBody = TestUtils.jsonFromRequest(networkDTO);
+    MvcResult result =
+        mockMvc
+            .perform(
+                MockMvcRequestBuilders.post(NETWORKS_URL)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+            .andExpect(status().isCreated())
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.name", is(networkDTO.getName())))
+            .andExpect(jsonPath("$.externalId", is(networkDTO.getExternalId())))
+            .andExpect(jsonPath("$.uri", is(networkDTO.getUri())))
+            .andExpect(jsonPath("$.contactEmail", is(networkDTO.getContactEmail())))
+            .andReturn();
+
+    long id = JsonPath.parse(result.getResponse().getContentAsString()).read("$.id", Long.class);
+    Optional<Network> network = networkRepository.findById(id);
+    assert network.isPresent();
+    assertEquals(networkDTO.getName(), network.get().getName());
+  }
+
+  @Test
+  public void testUpdate_Network_ok() throws Exception {
+    NetworkDTO networkDTO =
+        NetworkDTO.builder()
+            .id(1L)
+            .externalId("externalId")
+            .contactEmail("new@negotiator.com")
+            .name("newNetwork")
+            .uri("http://newuri.org")
+            .build();
+    String requestBody = TestUtils.jsonFromRequest(networkDTO);
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.put(NETWORKS_URL + "/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json"))
+        .andExpect(jsonPath("$.id", is(1)));
+
+    Optional<Network> network = networkRepository.findById(1L);
+    assert network.isPresent();
+    assertEquals(networkDTO.getName(), network.get().getName());
+  }
+
+  @Test
+  @Transactional
+  public void testAddResourcesToNetwork_ok() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post(NETWORKS_URL + "/1/resources")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("[7,8]"))
+        .andExpect(status().isOk());
+
+    Optional<Network> network = networkRepository.findById(1L);
+    assert network.isPresent();
+    assertTrue(network.get().getResources().stream().anyMatch(resource -> resource.getId() == 7));
+    assertTrue(network.get().getResources().stream().anyMatch(resource -> resource.getId() == 8));
+  }
+
+  @Test
+  @Transactional
+  public void testAddManagersToNetwork_ok() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post(NETWORKS_URL + "/1/managers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("[103,104]"))
+        .andExpect(status().isOk());
+
+    Optional<Network> network = networkRepository.findById(1L);
+    assert network.isPresent();
+    assertTrue(network.get().getManagers().stream().anyMatch(manager -> manager.getId() == 103));
+    assertTrue(network.get().getManagers().stream().anyMatch(manager -> manager.getId() == 104));
+  }
+
+  @Test
+  @Transactional
+  public void testRemoveResourceFromNetwork_ok() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.delete(NETWORKS_URL + "/1/resources/4"))
+        .andExpect(status().isNoContent());
+
+    Optional<Network> network = networkRepository.findById(1L);
+    assert network.isPresent();
+    assertTrue(network.get().getResources().stream().noneMatch(resource -> resource.getId() == 4));
+  }
+
+  @Test
+  @Transactional
+  public void testRemoveManagerFromNetwork_ok() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.delete(NETWORKS_URL + "/1/managers/101"))
+        .andExpect(status().isNoContent());
+
+    Optional<Network> network = networkRepository.findById(1L);
+    assert network.isPresent();
+    assertTrue(network.get().getManagers().stream().noneMatch(manager -> manager.getId() == 101));
   }
 }
