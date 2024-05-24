73fb7d157e456a8ef8851c3282371d148c7c8467
asulis <alessandro.sulis@crs4.it>
2024-05-24 14:11:20 +0200

    feat: Completes sync job implementation when the job started from an api call and makes Events consuming asynchronous

--- src/main/java/eu/bbmri_eric/negotiator/listeners/DiscoveryServiceSyncJobManager.java
+++ src/main/java/eu/bbmri_eric/negotiator/listeners/DiscoveryServiceSyncJobManager.java
@@ -4,6 +4,8 @@ import eu.bbmri_eric.negotiator.database.model.DiscoveryServiceSynchronizationJo
 import eu.bbmri_eric.negotiator.database.model.DiscoveryServiceSyncronizationJobStatus;
 import eu.bbmri_eric.negotiator.database.repository.DiscoveryServiceSynchronizationJobRepository;
 import eu.bbmri_eric.negotiator.events.DiscoveryServiceSynchronizationEvent;
+import eu.bbmri_eric.negotiator.exceptions.EntityNotStorableException;
+import eu.bbmri_eric.negotiator.service.BBMRIDirectoryServiceSynchClient;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -16,6 +18,8 @@ public class DiscoveryServiceSyncJobManager
   @Autowired
   private DiscoveryServiceSynchronizationJobRepository discoveryServiceSynchronizationJobRepository;
 
+  @Autowired private BBMRIDirectoryServiceSynchClient bbmriDirectoryServiceSyncClient;
+
   @Override
   public void onApplicationEvent(DiscoveryServiceSynchronizationEvent event) {
     log.info("Consuming spring custom event related to jobId - " + event.getJobId());
@@ -23,6 +27,16 @@ public class DiscoveryServiceSyncJobManager
     DiscoveryServiceSynchronizationJob job =
         discoveryServiceSynchronizationJobRepository.findDetailedById(event.getJobId()).get();
     job.setStatus(DiscoveryServiceSyncronizationJobStatus.IN_PROGRESS);
-    discoveryServiceSynchronizationJobRepository.save(job);
+    DiscoveryServiceSynchronizationJob savedJob =
+        discoveryServiceSynchronizationJobRepository.save(job);
+    try {
+      bbmriDirectoryServiceSyncClient.syncAllDiscoveryServiceObjects();
+      savedJob.setStatus(DiscoveryServiceSyncronizationJobStatus.COMPLETED);
+    } catch (EntityNotStorableException e) {
+      savedJob.setStatus(DiscoveryServiceSyncronizationJobStatus.FAILED);
+
+    } finally {
+      discoveryServiceSynchronizationJobRepository.save(job);
+    }
   }
 }
---
+++ src/main/java/eu/bbmri_eric/negotiator/service/BBMRIDirectoryServiceSynchClient.java
@@ -0,0 +1,7 @@
+package eu.bbmri_eric.negotiator.service;
+
+
+public interface BBMRIDirectoryServiceSynchClient {
+
+  public void syncAllDiscoveryServiceObjects();
+}
