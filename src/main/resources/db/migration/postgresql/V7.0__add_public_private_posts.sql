4900da2466a94f0313dece2f181599cccb3d5f54
Vittorio Meloni <vittorio.meloni@crs4.it>
2024-05-30 15:58:32 +0200

    feat: adds migration to add public and private posts to negotiation

---
+++ src/main/resources/db/migration/postgresql/V7.0__add_public_private_posts.sql
@@ -0,0 +1,15 @@
+ALTER TABLE negotiation RENAME COLUMN posts_enabled TO public_posts_enabled
+
+ALTER TABLE negotiation ADD COLUMN private_posts_enabled boolean
+
+UPDATE negotiation n
+SET public_posts_enabled = true
+WHERE n.current_state in ('SUBMITTED', 'APPROVED', 'DECLINED', 'IN_PROGRESS', 'PAUSED', 'CONCLUDED')
+
+UPDATE negotiation n
+SET private_posts_enabled = true
+WHERE n.current_state in ('APPROVED', 'IN_PROGRESS', 'PAUSED', 'CONCLUDED')
+
+UPDATE negotiation n
+SET private_posts_enabled = false
+WHERE n.current_state IN ('SUBMITTED', 'DECLINED', 'ABANDONED')
