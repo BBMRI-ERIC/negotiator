b30a44cdf3c1fbd37a60e4b14aba8cf6346711c4
asulis <alessandro.sulis@crs4.it>
2024-06-13 15:44:35 +0200

    refactor: Resolves some PR comments (Mapper for output dao, ease syncJob create method )

--- src/main/java/eu/bbmri_eric/negotiator/service/DiscoverySynchronizationJobServiceImpl.java
+++ src/main/java/eu/bbmri_eric/negotiator/service/DiscoverySynchronizationJobServiceImpl.java
@@ -6,10 +6,11 @@ import eu.bbmri_eric.negotiator.database.model.DiscoveryServiceSyncronizationJob
 import eu.bbmri_eric.negotiator.database.repository.DiscoveryServiceRepository;
 import eu.bbmri_eric.negotiator.database.repository.DiscoveryServiceSynchronizationJobRepository;
 import eu.bbmri_eric.negotiator.dto.syncjobservice.DiscoverySyncJobServiceDTO;
+import eu.bbmri_eric.negotiator.exceptions.EntityNotFoundException;
 import eu.bbmri_eric.negotiator.plugins.resourcesync.publishers.JobEventPublisher;
 import java.time.LocalDateTime;
-import java.util.Optional;
 import lombok.extern.apachecommons.CommonsLog;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,42 +25,28 @@ public class DiscoverySynchronizationJobServiceImpl implements DiscoverySynchron
   @Autowired
   DiscoveryServiceSynchronizationJobRepository discoveryServiceSynchronizationJobRepository;
 
+  ModelMapper modelMapper = new ModelMapper();
+
   @Override
   public DiscoverySyncJobServiceDTO createSyncJob(Long jobId) {
-    Optional<DiscoveryService> discoveryService = discoveryServiceRepository.findById(jobId);
-    if (!discoveryService.isEmpty()) {
-      LocalDateTime creationDate = LocalDateTime.now();
-      LocalDateTime modifyDate = LocalDateTime.now();
-      DiscoveryServiceSynchronizationJob job =
-          DiscoveryServiceSynchronizationJob.builder()
-              .service(discoveryService.get())
-              .creationDate(creationDate)
-              .modifiedDate(modifyDate)
-              .status(DiscoveryServiceSyncronizationJobStatus.SUBMITTED)
-              .build();
+    DiscoveryService discoveryService =
+        discoveryServiceRepository
+            .findById(jobId)
+            .orElseThrow(() -> new EntityNotFoundException(jobId));
+    LocalDateTime creationDate, modifyDate;
+    creationDate = modifyDate = LocalDateTime.now();
+    DiscoveryServiceSynchronizationJob job =
+        new DiscoveryServiceSynchronizationJob(
+            null,
+            discoveryService,
+            creationDate,
+            modifyDate,
+            DiscoveryServiceSyncronizationJobStatus.SUBMITTED);
+    log.debug("Saving new job entity....");
+    discoveryServiceSynchronizationJobRepository.save(job);
+    publisher.publishDiscoveryServiceSynchronizationEvent(job.getId(), jobId);
+    log.debug(String.format("Sync job event for discovery service %s properly published", jobId));
 
-      log.info("Saving new job entity....");
-      discoveryServiceSynchronizationJobRepository.save(job);
-
-      String message =
-          String.format(
-              "Sync Job %s properly instantiated for Discovery Service %s", job.getId(), jobId);
-      publisher.publishDiscoveryServiceSynchronizationEvent(job.getId(), jobId);
-      log.info(String.format("Sync job event for discovery service %s properly published", jobId));
-
-      return DiscoverySyncJobServiceDTO.builder()
-          .id(job.getId())
-          .discoveryServiceName(discoveryService.get().getName())
-          .creationDate(LocalDateTime.now())
-          .modifiedDate(LocalDateTime.now())
-          .message(message)
-          .build();
-    } else {
-      String message =
-          String.format(
-              "Impossible to create sync job: no such discovery service with id %s", jobId);
-      log.error(message);
-      throw new RuntimeException(message);
-    }
+    return modelMapper.map(job, DiscoverySyncJobServiceDTO.class);
   }
 }
