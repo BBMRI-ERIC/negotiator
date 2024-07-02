e760732318127ce9fb4fd1f498bcefe464130cce
Emilian Jungwirth <emilian.jung+github@gmail.com>
2024-07-02 16:13:29 +0200

    fix: doc conversion works again

--- src/main/java/eu/bbmri_eric/negotiator/service/AttachmentMergingService.java
+++ src/main/java/eu/bbmri_eric/negotiator/service/AttachmentMergingService.java
@@ -1,146 +1,117 @@
 package eu.bbmri_eric.negotiator.service;
 
 import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
+import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 import eu.bbmri_eric.negotiator.dto.attachments.AttachmentDTO;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType0Font;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.apache.poi.hwpf.usermodel.CharacterRun;
-import org.apache.poi.hwpf.usermodel.Paragraph;
 import org.apache.poi.hwpf.usermodel.Range;
-import org.docx4j.convert.out.FOSettings;
-import org.springframework.stereotype.Service;
 import org.docx4j.Docx4J;
+import org.docx4j.convert.out.FOSettings;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.springframework.stereotype.Service;
 
 @Service
 public class AttachmentMergingService {
 
-    private final AttachmentService attachmentService;
+  private final AttachmentService attachmentService;
 
-    public AttachmentMergingService(AttachmentService attachmentService) {
-        this.attachmentService = attachmentService;
+  public AttachmentMergingService(AttachmentService attachmentService) {
+    this.attachmentService = attachmentService;
+  }
+
+  /**
+   * Merge attachments to a PDF file.
+   *
+   * @param attachmentIds The list of attachment IDs to merge.
+   * @return The merged PDF file.
+   */
+  public byte[] mergeAttachmentsToPdf(List<String> attachmentIds) {
+    List<AttachmentDTO> attachmentsList =
+        attachmentIds.stream()
+            .map(id -> attachmentService.findById(id))
+            .collect(Collectors.toList());
+
+    List<byte[]> convertedAttachments = convertAttachmentsToPdf(attachmentsList);
+    byte[] mergedPdf = null;
+    try {
+      mergedPdf = PdfMergerService.mergePdfs(convertedAttachments);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to merge attachments to PDF", e);
     }
 
-    /**
-     * Merge attachments to a PDF file.
-     *
-     * @param attachmentIds The list of attachment IDs to merge.
-     * @return The merged PDF file.
-     */
-    public byte[] mergeAttachmentsToPdf(List<String> attachmentIds) {
-        List<AttachmentDTO> attachmentsList =
-                attachmentIds.stream()
-                        .map(id -> attachmentService.findById(id))
-                        .collect(Collectors.toList());
+    return mergedPdf;
+  }
 
-        List<byte[]> convertedAttachments = convertAttachmentsToPdf(attachmentsList);
-        byte[] mergedPdf = null;
-        try {
-            mergedPdf = PdfMergerService.mergePdfs(convertedAttachments);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to merge attachments to PDF", e);
-        }
-
-        return mergedPdf;
-    }
-
-    private List<byte[]> convertAttachmentsToPdf(List<AttachmentDTO> attachmentsList) {
-        return attachmentsList.stream()
-                .map(
-                        attachmentDTO -> {
-                            try {
-                                if (attachmentDTO.getContentType().equals("application/pdf")) {
-                                    return attachmentDTO.getPayload();
-                                } else if (attachmentDTO
-                                        .getContentType()
-                                        .equals(
-                                                "application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
-                                    return convertDocxToPdf(attachmentDTO.getPayload());
-                                } else if (attachmentDTO.getContentType().equals("application/msword")) {
-                                    return convertDocToPdf(attachmentDTO.getPayload());
-                                } else {
-                                    return null;
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                                return null;
-                            }
-                        })
-                .filter(pdfBytes -> pdfBytes != null)
-                .collect(Collectors.toList());
-    }
-
-    private byte[] convertDocToPdf(byte[] docBytes) throws IOException {
-        try (HWPFDocument doc = new HWPFDocument(new ByteArrayInputStream(docBytes));
-             ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
-             PDDocument pdfDoc = new PDDocument()) {
-
-            PDPage page = new PDPage();
-            pdfDoc.addPage(page);
-
-            try (PDPageContentStream contentStream = new PDPageContentStream(pdfDoc, page);
-                 WordExtractor extractor = new WordExtractor(doc)) {
-
-                Range range = doc.getRange();
-                contentStream.beginText();
-                contentStream.setLeading(14.5f);
-                contentStream.newLineAtOffset(25, 725);
-
-                for (int i = 0; i < range.numParagraphs(); i++) {
-                    Paragraph paragraph = range.getParagraph(i);
-                    for (int j = 0; j < paragraph.numCharacterRuns(); j++) {
-                        CharacterRun run = paragraph.getCharacterRun(j);
-                        String text = run.text();
-
-                        int fontSize = run.getFontSize() / 2;
-                        String fontFamily = run.getFontName();
-                        InputStream fontStream = getClass().getResourceAsStream("/fonts/" + fontFamily + ".ttf");
-                        PDType0Font pdfFont = PDType0Font.load(pdfDoc, fontStream);
-
-                        contentStream.setFont(pdfFont, fontSize);
-                        contentStream.showText(text.trim());
-                        contentStream.newLineAtOffset(0, -fontSize - 2);
-                    }
+  private List<byte[]> convertAttachmentsToPdf(List<AttachmentDTO> attachmentsList) {
+    return attachmentsList.stream()
+        .map(
+            attachmentDTO -> {
+              try {
+                if (attachmentDTO.getContentType().equals("application/pdf")) {
+                  return attachmentDTO.getPayload();
+                } else if (attachmentDTO
+                    .getContentType()
+                    .equals(
+                        "application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
+                  return convertDocxToPdf(attachmentDTO.getPayload());
+                } else if (attachmentDTO.getContentType().equals("application/msword")) {
+                  return convertDocToPdf(attachmentDTO.getPayload());
+                } else {
+                  return null;
                 }
+              } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+              }
+            })
+        .filter(pdfBytes -> pdfBytes != null)
+        .collect(Collectors.toList());
+  }
 
-                contentStream.endText();
-            }
+  private byte[] convertDocToPdf(byte[] docBytes) throws Exception {
+    Document pdfDoc = null;
 
-            pdfDoc.save(pdfOutputStream);
-            return pdfOutputStream.toByteArray();
-        }
+    try (ByteArrayInputStream docInputStream = new ByteArrayInputStream(docBytes);
+        HWPFDocument doc = new HWPFDocument(docInputStream);
+        ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream()) {
+
+      Range range = doc.getRange();
+
+      pdfDoc = new Document();
+      PdfWriter.getInstance(pdfDoc, pdfOutputStream);
+      pdfDoc.open();
+
+      for (int i = 0; i < range.numParagraphs(); i++) {
+        pdfDoc.add(new Paragraph(range.getParagraph(i).text()));
+      }
+      pdfDoc.close();
+      return pdfOutputStream.toByteArray();
+    } finally {
+      if (pdfDoc != null) {
+        pdfDoc.close();
+      }
     }
+  }
 
-    private byte[] convertDocxToPdf(byte[] docxBytes) throws Exception {
+  private byte[] convertDocxToPdf(byte[] docxBytes) throws Exception {
 
-        try (ByteArrayInputStream docxInputStream = new ByteArrayInputStream(docxBytes);
-             ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream()) {
+    try (ByteArrayInputStream docxInputStream = new ByteArrayInputStream(docxBytes);
+        ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream()) {
 
-            WordprocessingMLPackage wordMLPackage = Docx4J.load(docxInputStream);
+      WordprocessingMLPackage wordMLPackage = Docx4J.load(docxInputStream);
 
-            FOSettings foSettings = Docx4J.createFOSettings();
-            foSettings.setWmlPackage(wordMLPackage);
-            foSettings.setApacheFopMime("application/pdf");
+      FOSettings foSettings = Docx4J.createFOSettings();
+      foSettings.setWmlPackage(wordMLPackage);
+      foSettings.setApacheFopMime("application/pdf");
 
-            Docx4J.toFO(foSettings, pdfOutputStream, Docx4J.FLAG_EXPORT_PREFER_XSL);
-            return pdfOutputStream.toByteArray();
-        }
-
+      Docx4J.toFO(foSettings, pdfOutputStream, Docx4J.FLAG_EXPORT_PREFER_XSL);
+      return pdfOutputStream.toByteArray();
     }
+  }
 }
