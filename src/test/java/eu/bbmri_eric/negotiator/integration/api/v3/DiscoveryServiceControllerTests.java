4997c2d16e37de91a5e6047baff6f6db867e17f6
asulis <alessandro.sulis@crs4.it>
2024-07-18 11:14:01 +0200

    refactor: Reformats file

--- src/test/java/eu/bbmri_eric/negotiator/integration/api/v3/DiscoveryServiceControllerTests.java
+++ src/test/java/eu/bbmri_eric/negotiator/integration/api/v3/DiscoveryServiceControllerTests.java
@@ -25,8 +25,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
