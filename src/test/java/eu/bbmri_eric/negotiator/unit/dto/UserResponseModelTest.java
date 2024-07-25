b8991e4e447e9e82a4f712385b4578b5cb41c7ce
RadovanTomik <tomasik.radovan@gmail.com>
2024-07-25 17:39:22 +0200

    tests: add UserResponseModelTest

---
+++ src/test/java/eu/bbmri_eric/negotiator/unit/dto/UserResponseModelTest.java
@@ -0,0 +1,43 @@
+package eu.bbmri_eric.negotiator.unit.dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import eu.bbmri_eric.negotiator.dto.person.UserResponseModel;
+import org.junit.jupiter.api.Test;
+
+public class UserResponseModelTest {
+
+  @Test
+  public void testGetterAndSetter() {
+    UserResponseModel userResponseModel = new UserResponseModel();
+    userResponseModel.setId(Long.valueOf("12345"));
+    userResponseModel.setSubjectId("abc");
+    userResponseModel.setName("John Doe");
+    userResponseModel.setEmail("johndoe@example.com");
+    userResponseModel.setRepresentativeOfAnyResource(true);
+
+    assertEquals("12345", userResponseModel.getId());
+    assertEquals("abc", userResponseModel.getSubjectId());
+    assertEquals("John Doe", userResponseModel.getName());
+    assertEquals("johndoe@example.com", userResponseModel.getEmail());
+    assertTrue(userResponseModel.isRepresentativeOfAnyResource());
+  }
+
+  @Test
+  public void testEqualsAndHashCode() {
+    UserResponseModel user1 = new UserResponseModel();
+    user1.setId(Long.valueOf("12345"));
+    user1.setSubjectId("abc");
+    user1.setName("John Doe");
+    user1.setEmail("johndoe@example.com");
+
+    UserResponseModel user2 = new UserResponseModel();
+    user2.setId(Long.valueOf("12345"));
+    user2.setSubjectId("abc");
+    user2.setName("John Doe");
+    user2.setEmail("johndoe@example.com");
+
+    assertTrue(user1.equals(user2) && user2.equals(user1));
+    assertEquals(user1.hashCode(), user2.hashCode());
+  }
+}
