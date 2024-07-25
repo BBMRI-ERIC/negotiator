caec9ed8a355275121412c92c9443b438161de4f
RadovanTomik <tomasik.radovan@gmail.com>
2024-07-25 08:54:00 +0200

    chore: reformat InfoRequirement hash code and equals

--- src/test/java/eu/bbmri_eric/negotiator/integration/service/InformationRequirementServiceTest.java
+++ src/test/java/eu/bbmri_eric/negotiator/integration/service/InformationRequirementServiceTest.java
@@ -14,10 +14,12 @@ import eu.bbmri_eric.negotiator.service.InformationRequirementService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class InformationRequirementServiceTest {
   @Autowired private InformationRequirementService service;
   @Autowired private AccessFormRepository accessFormRepository;
