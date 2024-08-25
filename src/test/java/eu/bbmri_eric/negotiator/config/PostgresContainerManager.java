f8fe810467bd8fdf277b2241bb89a643e6a03fd4
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-08-25 09:33:05 +0200

    tests: cleanup testcontainers setup
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/test/java/eu/bbmri_eric/negotiator/config/PostgresContainerManager.java
+++ src/test/java/eu/bbmri_eric/negotiator/config/PostgresContainerManager.java
@@ -2,21 +2,17 @@ package eu.bbmri_eric.negotiator.config;
 
 import org.testcontainers.containers.PostgreSQLContainer;
 
+/** Class for managing the Postgres container used in testing. */
 public class PostgresContainerManager {
   private static PostgreSQLContainer<?> container;
 
   public static PostgreSQLContainer<?> getContainer() {
     if (container == null) {
       container = new PostgreSQLContainer<>("postgres:16-alpine");
+      // The container should be automatically stopped
+      // when the Testcontainers orchestration container is terminated by Junit.
       container.start();
     }
     return container;
-  }
-
-  public static void stopContainer() {
-    if (container != null) {
-      container.stop();
-      container = null;
-    }
   }
 }
