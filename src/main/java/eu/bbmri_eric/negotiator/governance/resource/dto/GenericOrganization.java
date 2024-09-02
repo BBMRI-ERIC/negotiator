3235f439a3ce3690a16b4781597181da46cebe37
asulis <alessandro.sulis@crs4.it>
2024-09-02 16:14:49 +0200

    refactor: Fixes last PR's comments

--- src/main/java/eu/bbmri_eric/negotiator/governance/resource/dto/GenericOrganization.java
+++ src/main/java/eu/bbmri_eric/negotiator/governance/resource/dto/GenericOrganization.java
@@ -1,7 +1,18 @@
 package eu.bbmri_eric.negotiator.governance.resource.dto;
 
+/** Generic interface for Organizations * */
 public interface GenericOrganization {
+  /**
+   * Gets the unique identifier of the Organization
+   *
+   * @return The unique Id of the Organization
+   */
   String getId();
 
+  /**
+   * Gets the name of the Organization
+   *
+   * @return The name of the Organization
+   */
   String getName();
 }
