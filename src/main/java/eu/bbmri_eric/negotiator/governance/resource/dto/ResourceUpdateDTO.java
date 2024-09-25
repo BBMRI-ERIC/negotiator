95671791c26d8947fef1730aa4fbd788846aad50
asulis <alessandro.sulis@crs4.it>
2024-09-25 09:19:06 +0200

    refactor: Removes NotNull constraint in the resource update DTO

--- src/main/java/eu/bbmri_eric/negotiator/governance/resource/dto/ResourceUpdateDTO.java
+++ src/main/java/eu/bbmri_eric/negotiator/governance/resource/dto/ResourceUpdateDTO.java
@@ -1,6 +1,5 @@
 package eu.bbmri_eric.negotiator.governance.resource.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +13,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ResourceUpdateDTO {
 
-  @NotNull private String name;
+  private String name;
   private String description;
 }
