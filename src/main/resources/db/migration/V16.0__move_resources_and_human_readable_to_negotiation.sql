3ec5e83efcab0fb17bba4aaf733545ddf5ab0641
Vittorio Meloni <vittorio.meloni@crs4.it>
2024-09-25 10:30:07 +0200

    refactor: adds data migration statement to flyway script

--- src/main/resources/db/migration/V16.0__move_resources_and_human_readable_to_negotiation.sql
+++ src/main/resources/db/migration/V16.0__move_resources_and_human_readable_to_negotiation.sql
@@ -1,7 +1,18 @@
-ALTER TABLE negotiation ADD COLUMN human_readable text NOT NULL;
+alter table negotiation
+add column human_readable text not null default = ''
 
-CREATE TABLE negotiation_resources_link (
-    negotiation_id character varying(255) NOT NULL REFERENCES negotiation(id),
-    resource_id bigint NOT NULL REFERENCES resource(id),
-    PRIMARY KEY (negotiation_id, resource_id)
-);
+create table negotiation_resources_link (
+    negotiation_id character varying(255) not null references negotiation(id),
+    resource_id bigint not null references resource(id),
+    primary key (negotiation_id, resource_id)
+)
+
+update negotiation
+set human_readable = (
+	select human_readable
+	from request
+	where negotiation.id = request.negotiation_id
+)
+
+insert into negotiation_resources_link (negotiation_id, resource_id)
+select r.negotiation_id, rrl.resource_id from request_resources_link rrl join request r on rrl.request_id = r.id
