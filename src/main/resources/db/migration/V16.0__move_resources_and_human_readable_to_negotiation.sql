6e68fb07ae0a9cd7c93cdc63ec2eac6e23bfd289
Vittorio Meloni <vittorio.meloni@crs4.it>
2024-09-25 10:41:31 +0200

    refactor(db): fix the flyway migration script

--- src/main/resources/db/migration/V16.0__move_resources_and_human_readable_to_negotiation.sql
+++ src/main/resources/db/migration/V16.0__move_resources_and_human_readable_to_negotiation.sql
@@ -1,18 +1,18 @@
 alter table negotiation
-add column human_readable text not null default = ''
+add column human_readable text not null;
 
 create table negotiation_resources_link (
     negotiation_id character varying(255) not null references negotiation(id),
     resource_id bigint not null references resource(id),
     primary key (negotiation_id, resource_id)
-)
+);
 
 update negotiation
 set human_readable = (
 	select human_readable
 	from request
 	where negotiation.id = request.negotiation_id
-)
+);
 
 insert into negotiation_resources_link (negotiation_id, resource_id)
-select r.negotiation_id, rrl.resource_id from request_resources_link rrl join request r on rrl.request_id = r.id
+select r.negotiation_id, rrl.resource_id from request_resources_link rrl join request r on rrl.request_id = r.id;
