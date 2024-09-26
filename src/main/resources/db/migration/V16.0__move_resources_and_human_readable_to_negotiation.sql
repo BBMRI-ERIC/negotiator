1f3db98f82ce461dddf9e50843fbe5907cf73796
Vittorio Meloni <vittorio.meloni@crs4.it>
2024-09-26 09:18:12 +0200

    refactor: removes the default values from new negotiation's columns

--- src/main/resources/db/migration/V16.0__move_resources_and_human_readable_to_negotiation.sql
+++ src/main/resources/db/migration/V16.0__move_resources_and_human_readable_to_negotiation.sql
@@ -1,7 +1,6 @@
 alter table negotiation
-add column human_readable text not null default '',
-add column discovery_service_id bigint not null default 1,
-
+add column human_readable text,
+add column discovery_service_id bigint,
 add constraint fkey_discovery_service_id FOREIGN KEY (discovery_service_id) REFERENCES discovery_service(id);
 
 create table negotiation_resource_link (
@@ -18,9 +17,12 @@ set (human_readable, discovery_service_id) = (
 	where negotiation.id = request.negotiation_id
 );
 
+alter table negotiation alter column human_readable set not null;
+alter table negotiation alter column discovery_service_id set not null;
+
 insert into negotiation_resource_link (negotiation_id, resource_id)
 select r.negotiation_id, rrl.resource_id from request_resources_link rrl join request r on rrl.request_id = r.id;
 
 delete from request where negotiation_id is null;
-
 alter table request drop column negotiation_id;
+
