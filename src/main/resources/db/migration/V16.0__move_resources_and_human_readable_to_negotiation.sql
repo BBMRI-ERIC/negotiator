9be77e62e86099d068eb9f11981df156c7765d04
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-09-26 17:05:46 +0200

    wip: move resource status to resource links
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/main/resources/db/migration/V16.0__move_resources_and_human_readable_to_negotiation.sql
+++ src/main/resources/db/migration/V16.0__move_resources_and_human_readable_to_negotiation.sql
@@ -15,19 +15,27 @@ update negotiation
 set (human_readable, discovery_service_id) = (select human_readable, discovery_service_id
                                               from request
                                               where negotiation.id = request.negotiation_id);
+update negotiation
+set discovery_service_id = 1
+where negotiation.discovery_service_id is null;
+alter table negotiation
+    alter column discovery_service_id set not null;
+INSERT INTO negotiation_resource_link (negotiation_id, resource_id, current_state)
+SELECT n.id, r.id, rspn.current_state
+FROM resource_state_per_negotiation rspn
+         JOIN public.resource r ON r.source_id = rspn.resource_id
+         JOIN public.negotiation n ON n.id = rspn.negotiation_id
+         JOIN public.request r2 ON n.id = r2.negotiation_id;
+INSERT INTO negotiation_resource_link (negotiation_id, resource_id, current_state)
+SELECT n.id, r2.id, NULL
+FROM negotiation n
+         JOIN public.request r ON n.id = r.negotiation_id
+         JOIN public.request_resources_link rrl ON r.id = rrl.request_id
+         JOIN public.resource r2 ON r2.id = rrl.resource_id
+where n.current_state = 'SUBMITTED'
+   or n.current_state = 'DECLINED';
 
-alter table negotiation alter column discovery_service_id set not null;
 
-insert into negotiation_resource_link (negotiation_id, resource_id, current_state)
-select r.negotiation_id, rrl.resource_id, rspn.current_state
-from request_resources_link rrl
-         join request r on rrl.request_id = r.id
-         join public.resource r2 on r2.id = rrl.resource_id
-         join public.resource_state_per_negotiation rspn on r2.source_id = rspn.resource_id;
-
-delete
-from request
-where negotiation_id is null;
 alter table request
     drop column negotiation_id;
 drop table resource_state_per_negotiation;
