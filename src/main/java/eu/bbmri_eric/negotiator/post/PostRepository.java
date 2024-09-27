9f1cb137b47d7e7d00737698fe918b4fe1898f35
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-09-27 17:21:25 +0200

    refactor: clean up post entity and
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/main/java/eu/bbmri_eric/negotiator/post/PostRepository.java
+++ src/main/java/eu/bbmri_eric/negotiator/post/PostRepository.java
@@ -1,24 +1,14 @@
 package eu.bbmri_eric.negotiator.post;
 
 import java.util.List;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, String> {
-
-  @EntityGraph("post-with-details")
+  
   List<Post> findByNegotiationId(String negotiationId);
-
-  @EntityGraph("post-with-details")
-  List<Post> findByNegotiationIdAndType(String negotiationId, PostType type);
-
-  @EntityGraph("post-with-details")
-  List<Post> findByNegotiationIdAndOrganizationId(String negotiationId, Long organizationId);
-
-  @EntityGraph("post-with-details")
+  
   Post findByIdAndNegotiationId(String negotiationId, String id);
-
-  @EntityGraph("post-with-details")
+  
   List<Post> findByNegotiationIdAndTypeAndOrganization_ExternalId(
       String negotiationId, PostType type, String organizationId);
 }
