1894bc7c34568f8ba7b3407f3abf4149a4174b3c
Vittorio Meloni <vittorio.meloni@crs4.it>
2024-10-14 16:05:51 +0200

    feat: adds put endpoint for ui-config

--- src/main/java/eu/bbmri_eric/negotiator/settings/UIConfigService.java
+++ src/main/java/eu/bbmri_eric/negotiator/settings/UIConfigService.java
@@ -1,8 +1,9 @@
 package eu.bbmri_eric.negotiator.settings;
 
-import java.util.List;
 import java.util.Map;
 
 public interface UIConfigService {
-  Map<String, List<Map<String, Object>>> getAllParameters();
+  Map<String, Map<String, Object>> getAllParameters();
+
+  void updateParameters(Map<String, Map<String, Object>> parameters);
 }
--- src/main/java/eu/bbmri_eric/negotiator/settings/UIConfigServiceImpl.java
+++ src/main/java/eu/bbmri_eric/negotiator/settings/UIConfigServiceImpl.java
@@ -1,12 +1,12 @@
 package eu.bbmri_eric.negotiator.settings;
 
-import java.util.ArrayList;
+import eu.bbmri_eric.negotiator.common.exceptions.WrongRequestException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Service;
 
-@Service()
+@Service(value = "DefaultUIConfigService")
 public class UIConfigServiceImpl implements UIConfigService {
 
   private final UIParameterRepository uiParameterRepository;
@@ -16,29 +16,65 @@ public class UIConfigServiceImpl implements UIConfigService {
   }
 
   @Override
-  public Map<String, List<Map<String, Object>>> getAllParameters() {
+  public Map<String, Map<String, Object>> getAllParameters() {
     List<UIParameter> parameters = uiParameterRepository.findAll();
 
-    Map<String, List<Map<String, Object>>> settings = new HashMap<>();
+    Map<String, Map<String, Object>> settings = new HashMap<>();
 
     parameters.forEach(
         uiParameter -> {
-          Map<String, Object> parameter = new HashMap<>();
-          switch (uiParameter.type) {
+          switch (uiParameter.getType()) {
             case INT:
-              parameter.put(uiParameter.getName(), Integer.valueOf(uiParameter.getValue()));
+              settings
+                  .computeIfAbsent(uiParameter.getCategory(), k -> new HashMap<>())
+                  .put(uiParameter.getName(), Integer.valueOf(uiParameter.getValue()));
               break;
             case BOOL:
-              parameter.put(uiParameter.getName(), Boolean.valueOf(uiParameter.getValue()));
+              settings
+                  .computeIfAbsent(uiParameter.getCategory(), k -> new HashMap<>())
+                  .put(uiParameter.getName(), Boolean.valueOf(uiParameter.getValue()));
               break;
             default:
-              parameter.put(uiParameter.getName(), uiParameter.getValue());
+              settings
+                  .computeIfAbsent(uiParameter.getCategory(), k -> new HashMap<>())
+                  .put(uiParameter.getName(), uiParameter.getValue());
               break;
           }
-          settings
-              .computeIfAbsent(uiParameter.getCategory(), k -> new ArrayList<>())
-              .add(parameter);
         });
     return settings;
+  }
+
+  @Override
+  public void updateParameters(Map<String, Map<String, Object>> categories) {
+    categories.forEach(
+        (category, parameters) -> {
+          parameters.forEach(
+              (name, value) -> {
+                UIParameter uiParameter =
+                    uiParameterRepository.findByCategoryAndName(category, name);
+                String stringValue = String.valueOf(value);
+                switch (uiParameter.getType()) {
+                  case INT:
+                    try {
+                      Integer.valueOf(stringValue);
+                    } catch (NumberFormatException ex) {
+                      throw new WrongRequestException(
+                          "Value %s cannot be saved as INT".formatted(uiParameter.getValue()));
+                    }
+                    break;
+                  case BOOL:
+                    String lower = stringValue.toLowerCase();
+                    if (!lower.equals("false") && !lower.equals("true")) {
+                      throw new WrongRequestException(
+                          "Value %s cannot be saved as BOOL".formatted(uiParameter.getValue()));
+                    }
+                    break;
+                  default:
+                    break;
+                }
+                uiParameter.setValue(stringValue);
+                uiParameterRepository.save(uiParameter);
+              });
+        });
   }
 }
