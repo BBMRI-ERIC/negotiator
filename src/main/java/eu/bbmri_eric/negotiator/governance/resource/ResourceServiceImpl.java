d39eabcc97be4ce480faace3a1069f473a6e45db
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-10-25 08:35:18 +0200

    refactor: remove unnecessary logging
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/main/java/eu/bbmri_eric/negotiator/governance/resource/ResourceServiceImpl.java
+++ src/main/java/eu/bbmri_eric/negotiator/governance/resource/ResourceServiceImpl.java
@@ -163,10 +163,8 @@ public class ResourceServiceImpl implements ResourceService {
         }
       }
     } else {
-      log.error("here");
       for (Resource resource : resourcesToUpdate) {
         negotiation.setStateForResource(resource.getSourceId(), state);
-        log.error(negotiation.getCurrentStateForResource(resource.getSourceId()));
       }
     }
   }
