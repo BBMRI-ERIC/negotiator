2477abdf45aca2b7920d0e4c3599e6d5b92cef61
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2024-11-18 16:42:27 +0100

    chore: update swagger documentation
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- src/main/java/eu/bbmri_eric/negotiator/negotiation/dto/NegotiationDTO.java
+++ src/main/java/eu/bbmri_eric/negotiator/negotiation/dto/NegotiationDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.JsonNode;
 import eu.bbmri_eric.negotiator.user.UserResponseModel;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -22,15 +23,55 @@ import org.springframework.hateoas.server.core.Relation;
 @Relation(collectionRelation = "negotiations", itemRelation = "negotiation")
 public class NegotiationDTO {
 
-  @NotNull private String id;
+  @NotNull
+  @Schema(
+      description = "Unique identifier of the negotiation",
+      example = "123e4567-e89b-12d3-a456-426614174000")
+  private String id;
+
+  @Schema(description = "Details about the author of the negotiation")
   private UserResponseModel author;
-  @NotNull private JsonNode payload;
-  @NotNull private String status;
-  @NotNull private String humanReadable;
-  @NotNull private boolean publicPostsEnabled;
-  @NotNull private boolean privatePostsEnabled;
-  @NotNull private LocalDateTime creationDate;
-  @NotNull private LocalDateTime modifiedDate;
+
+  @NotNull
+  @Schema(
+      description = "Payload containing additional negotiation data",
+      example = "{ \"key\": \"value\" }",
+      type = "object")
+  private JsonNode payload;
+
+  @NotNull
+  @Schema(description = "Current status of the negotiation", example = "PENDING")
+  private String status;
+
+  @NotNull
+  @Schema(
+      description = "Human-readable description of the negotiation",
+      example = "Negotiation between parties X and Y")
+  private String humanReadable;
+
+  @NotNull
+  @Schema(
+      description = "Indicates if public posts are enabled for this negotiation",
+      example = "true")
+  private boolean publicPostsEnabled;
+
+  @NotNull
+  @Schema(
+      description = "Indicates if private posts are enabled for this negotiation",
+      example = "false")
+  private boolean privatePostsEnabled;
+
+  @NotNull
+  @Schema(
+      description = "The timestamp when the negotiation was created",
+      example = "2024-11-18T12:00:00")
+  private LocalDateTime creationDate;
+
+  @NotNull
+  @Schema(
+      description = "The timestamp when the negotiation was last modified",
+      example = "2024-11-18T15:00:00")
+  private LocalDateTime modifiedDate;
 
   @JsonIgnore
   public String getStatusForResource(String resourceId) {
