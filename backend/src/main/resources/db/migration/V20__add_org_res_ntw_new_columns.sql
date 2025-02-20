4655039dfea49753d8936420d078d744cc4a7d7f
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2025-02-20 08:31:14 +0100

    fix: revert migration file changes
    Signed-off-by: RadovanTomik <radovan.tomasik@bbmri-eric.eu>

--- backend/src/main/resources/db/migration/V20__add_org_res_ntw_new_columns.sql
+++ backend/src/main/resources/db/migration/V20__add_org_res_ntw_new_columns.sql
@@ -8,7 +8,4 @@ alter table resource add column contact_email varchar(255);
 
 alter table resource add column uri varchar(255);
 
-alter table resource
-    add column withdrawn bool DEFAULT false;
-
 alter table network add column description varchar(5000);
