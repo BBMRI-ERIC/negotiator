bd4e12651de8163e827afb81c0e4fdb3f3a611b5
RadovanTomik <tomasik.radovan@gmail.com>
2025-02-25 16:42:02 +0100

    refactor: add util method for executing tasks as a system user

--- backend/src/main/java/eu/bbmri_eric/negotiator/common/AuthenticatedUserContext.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/common/AuthenticatedUserContext.java
@@ -1,12 +1,19 @@
 package eu.bbmri_eric.negotiator.common;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Collectors;
+
+import eu.bbmri_eric.negotiator.user.Person;
+import eu.bbmri_eric.negotiator.user.PersonRepository;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +21,15 @@ import org.springframework.stereotype.Component;
 @Component
 @CommonsLog
 public class AuthenticatedUserContext {
-  /**
+
+  private final PersonRepository personRepository;
+
+    public AuthenticatedUserContext(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+
+    /**
    * Retrieve an internal identifier for the currently Authenticated user.
    *
    * @return the internal identifier
@@ -27,6 +42,30 @@ public class AuthenticatedUserContext {
     } catch (Exception e) {
       log.error(e.getMessage());
       throw new AuthenticationCredentialsNotFoundException("No authenticated user found");
+    }
+  }
+
+  /**
+   * Method for temporary elevation of auth, useful for running automatic operations.
+   * @param task a runnable method
+   */
+  public void runAsSystemUser(Runnable task) {
+    SecurityContext originalContext = SecurityContextHolder.getContext();
+    try {
+      Person person = personRepository.findById(0L).get();
+      Authentication systemAuth =
+              new UsernamePasswordAuthenticationToken(
+                      new UserPrincipal(person), null, List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+      SecurityContext newContext = SecurityContextHolder.createEmptyContext();
+      newContext.setAuthentication(systemAuth);
+      SecurityContextHolder.setContext(newContext);
+      task.run();
+    }
+    catch (NoSuchElementException e){
+      log.error("Could not execute automated actions as the system user does not exist");
+    }
+    finally {
+      SecurityContextHolder.setContext(originalContext);
     }
   }
 
