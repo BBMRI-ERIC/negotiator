497aa8a1dfcb965dd6b2e967471bac447fdcf62c
Milos <tmilos94@gmail.com>
2025-03-14 12:33:55 +0100

    fix: remove bootstrap-vue-next and unplugin-vue-components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,6 @@ import { createPinia } from 'pinia'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faDownload, faPencil, faSpinner, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import matomo from './config/matomo.js'
@@ -16,6 +14,7 @@ import 'vue3-tour/dist/vue3-tour.css'
 import { useOidcStore } from './store/oidc'
 import { piniaOidcCreateRouterMiddleware } from 'pinia-oidc'
 import VueDOMPurifyHTML from 'vue-dompurify-html';
+import('./assets/scss/theme.scss')
 
 library.add(faSpinner)
 library.add(faPencil)
@@ -51,5 +50,3 @@ app.mount('#app')
 if (matomo.matomoHost !== 'MATOMO_HOST_PLACEHOLDER') {
   window._paq.push(['trackPageView']) // To track a page view
 }
-
-import('./assets/scss/theme.scss')
