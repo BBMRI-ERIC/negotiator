1535f2d5cd66cb60796df79c2471fc102800464e
Emilian Jungwirth <emilian.jung+github@gmail.com>
2025-03-26 11:42:34 +0100

    feat: adds endpoints to get, set and reset email templates

--- backend/src/main/java/eu/bbmri_eric/negotiator/notification/NotificationController.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/notification/NotificationController.java
@@ -8,10 +8,7 @@ import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v3")
@@ -27,5 +24,23 @@ public class NotificationController {
   public List<NotificationDTO> getNotificationsForUser() {
     return userNotificationService.getNotificationsForUser(
         AuthenticatedUserContext.getCurrentlyAuthenticatedUserInternalId());
+  }
+
+  @GetMapping(value = "/notifications/template/{templateName}", produces = MediaType.APPLICATION_XHTML_XML_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  public String getNotificationTemplate(@PathVariable String templateName) {
+    return userNotificationService.getNotificationTemplate(templateName);
+  }
+
+  @PostMapping(value = "/notifications/template/{templateName}", produces = MediaType.APPLICATION_XHTML_XML_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  public String updateNotificationTemplate(@PathVariable String templateName, @RequestBody String template) {
+    return userNotificationService.updateNotificationTemplate(templateName, template);
+  }
+
+  @PostMapping(value = "/notifications/template/{templateName}/reset", produces = MediaType.APPLICATION_XHTML_XML_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  public String updateNotificationTemplate(@PathVariable String templateName) {
+    return userNotificationService.resetNotificationTemplate(templateName);
   }
 }
