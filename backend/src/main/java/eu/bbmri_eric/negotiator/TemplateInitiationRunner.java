b15a1e41809295eca807edf81a9047d1ae8f6720
Emilian Jungwirth <emilian.jung+github@gmail.com>
2025-03-27 15:22:17 +0100

    chore: linting

--- backend/src/main/java/eu/bbmri_eric/negotiator/TemplateInitiationRunner.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/TemplateInitiationRunner.java
@@ -1,47 +1,49 @@
 package eu.bbmri_eric.negotiator;
 
-
 import eu.bbmri_eric.negotiator.notification.UserNotificationService;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
-
 @Component
 public class TemplateInitiationRunner implements CommandLineRunner {
 
-    private final UserNotificationService userNotificationService;
-    @Value("${spring.thymeleaf.prefix:classpath:/templates/}")
-    private String thymeleafPrefix;
+  private final UserNotificationService userNotificationService;
 
-    @Value("${spring.thymeleaf.suffix:.html}")
-    private String thymeleafSuffix;
+  @Value("${spring.thymeleaf.prefix:classpath:/templates/}")
+  private String thymeleafPrefix;
 
-    @Autowired
-    public TemplateInitiationRunner(UserNotificationService userNotificationService) {
-        this.userNotificationService = userNotificationService;
+  @Value("${spring.thymeleaf.suffix:.html}")
+  private String thymeleafSuffix;
+
+  @Autowired
+  public TemplateInitiationRunner(UserNotificationService userNotificationService) {
+    this.userNotificationService = userNotificationService;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    String cleanedPrefix = thymeleafPrefix.replaceFirst("^file:(//)?", "");
+    Path templatesDir = Paths.get(cleanedPrefix).toAbsolutePath().normalize();
+    try (Stream<Path> paths = Files.walk(templatesDir)) {
+      paths
+          .filter(Files::isRegularFile)
+          .filter(path -> path.toString().endsWith(thymeleafSuffix + ".default"))
+          .forEach(
+              path ->
+                  resetTemplateIfNotExists(path.getFileName().toString().replace(".default", "")));
     }
+  }
 
-    @Override
-    public void run(String... args) throws Exception {
-        String cleanedPrefix = thymeleafPrefix.replaceFirst("^file:(//)?", "");
-        Path templatesDir = Paths.get(cleanedPrefix).toAbsolutePath().normalize();
-        try (Stream<Path> paths = Files.walk(templatesDir)) {
-            paths.filter(Files::isRegularFile)
-                    .filter(path -> path.toString().endsWith(thymeleafSuffix + ".default"))
-                    .forEach(path -> resetTemplateIfNotExists(path.getFileName().toString().replace(".default", "")));
-        }
+  private void resetTemplateIfNotExists(String templateFile) {
+    String cleanedPrefix = thymeleafPrefix.replaceFirst("^file:(//)?", "");
+    if (!Files.exists(Paths.get(cleanedPrefix + templateFile))) {
+      userNotificationService.resetNotificationTemplate(templateFile.replace(thymeleafSuffix, ""));
     }
-
-    private void resetTemplateIfNotExists(String templateFile) {
-        String cleanedPrefix = thymeleafPrefix.replaceFirst("^file:(//)?", "");
-        if (!Files.exists(Paths.get(cleanedPrefix+templateFile))) {
-            userNotificationService.resetNotificationTemplate(templateFile.replace(thymeleafSuffix,""));
-        }
-    }
+  }
 }
