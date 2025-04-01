330af608a771d9e9f3675c84e5dd3be71cb73d18
Emilian Jungwirth <emilian.jung+github@gmail.com>
2025-04-01 09:11:30 +0200

    test: adds test for defaultTemplaePath

---
+++ backend/src/test/java/eu/bbmri_eric/negotiator/integration/service/UserNotificationServiceUsesDefaultThymeleafPrefixTest.java
@@ -0,0 +1,37 @@
+package eu.bbmri_eric.negotiator.integration.service;
+
+import eu.bbmri_eric.negotiator.common.exceptions.ForbiddenRequestException;
+import eu.bbmri_eric.negotiator.notification.UserNotificationService;
+import eu.bbmri_eric.negotiator.util.IntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@IntegrationTest(loadTestData = true)
+@TestPropertySource(properties = "spring.thymeleaf.prefix=classpath:/templates/")
+public class UserNotificationServiceUsesDefaultThymeleafPrefixTest {
+
+    @Autowired
+    private UserNotificationService userNotificationService;
+
+    @Test
+    void updateNotificationTemplate_defaultTemplate_throwsForbiddenRequestException() {
+        String templateName = "footer";
+        String newTemplateContent = "<html><body>Updated Template</body></html>";
+
+        assertThrows(
+                ForbiddenRequestException.class,
+                () -> userNotificationService.updateNotificationTemplate(templateName, newTemplateContent));
+    }
+
+    @Test
+    void resetNotificationTemplate_defaultTemplate_throwsForbiddenRequestException() {
+        String templateName = "footer";
+
+        assertThrows(
+                ForbiddenRequestException.class,
+                () -> userNotificationService.resetNotificationTemplate(templateName));
+    }
+}
