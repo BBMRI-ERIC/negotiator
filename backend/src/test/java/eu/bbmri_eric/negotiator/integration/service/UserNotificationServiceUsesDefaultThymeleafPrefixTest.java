0255d48171d2b0a73086208bc46abfa9500cb2a9
Emilian Jungwirth <emilian.jung+github@gmail.com>
2025-04-01 09:12:02 +0200

    chore: linting

--- backend/src/test/java/eu/bbmri_eric/negotiator/integration/service/UserNotificationServiceUsesDefaultThymeleafPrefixTest.java
+++ backend/src/test/java/eu/bbmri_eric/negotiator/integration/service/UserNotificationServiceUsesDefaultThymeleafPrefixTest.java
@@ -1,5 +1,7 @@
 package eu.bbmri_eric.negotiator.integration.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import eu.bbmri_eric.negotiator.common.exceptions.ForbiddenRequestException;
 import eu.bbmri_eric.negotiator.notification.UserNotificationService;
 import eu.bbmri_eric.negotiator.util.IntegrationTest;
@@ -7,31 +9,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 @IntegrationTest(loadTestData = true)
 @TestPropertySource(properties = "spring.thymeleaf.prefix=classpath:/templates/")
 public class UserNotificationServiceUsesDefaultThymeleafPrefixTest {
 
-    @Autowired
-    private UserNotificationService userNotificationService;
+  @Autowired private UserNotificationService userNotificationService;
 
-    @Test
-    void updateNotificationTemplate_defaultTemplate_throwsForbiddenRequestException() {
-        String templateName = "footer";
-        String newTemplateContent = "<html><body>Updated Template</body></html>";
+  @Test
+  void updateNotificationTemplate_defaultTemplate_throwsForbiddenRequestException() {
+    String templateName = "footer";
+    String newTemplateContent = "<html><body>Updated Template</body></html>";
 
-        assertThrows(
-                ForbiddenRequestException.class,
-                () -> userNotificationService.updateNotificationTemplate(templateName, newTemplateContent));
-    }
+    assertThrows(
+        ForbiddenRequestException.class,
+        () -> userNotificationService.updateNotificationTemplate(templateName, newTemplateContent));
+  }
 
-    @Test
-    void resetNotificationTemplate_defaultTemplate_throwsForbiddenRequestException() {
-        String templateName = "footer";
+  @Test
+  void resetNotificationTemplate_defaultTemplate_throwsForbiddenRequestException() {
+    String templateName = "footer";
 
-        assertThrows(
-                ForbiddenRequestException.class,
-                () -> userNotificationService.resetNotificationTemplate(templateName));
-    }
+    assertThrows(
+        ForbiddenRequestException.class,
+        () -> userNotificationService.resetNotificationTemplate(templateName));
+  }
 }
