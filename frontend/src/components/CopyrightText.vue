5ee2c29dca1cd4d473b0a450723c9e238d7ba72e
Milos <tmilos94@gmail.com>
2025-04-07 12:30:18 +0200

    feat: add component for copyright text

---
+++ frontend/src/components/CopyrightText.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>&copy; {{ returnCurrentYear }} BBMRI-ERIC</p>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import moment from 'moment'
+
+const returnCurrentYear = computed(() => {
+  return moment().format('YYYY') || '2025'
+})
+</script>
