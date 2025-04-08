1216ef43ce7eff1c1b5cddf0cfbbe6c424dcd786
Milos <tmilos94@gmail.com>
2025-04-08 13:56:53 +0200

    feat: add feature flag

--- frontend/src/config/featureFlags.js
+++ frontend/src/config/featureFlags.js
@@ -3,7 +3,8 @@ const devSettings = {
   vueTour: true,
   notifications: true,
   networks: true,
-  dev_mode: true
+  add_acess_form: true,
+  dev_mode: true,
 }
 
 const prodSettings = {
@@ -11,7 +12,8 @@ const prodSettings = {
   vueTour: 'FEATURE_FLAG_VUETOUR_PLACEHOLDER',
   notifications: 'FEATURE_FLAG_NOTIFICATIONS',
   networks: 'FEATURE_FLAG_NETWORKS',
-  dev_mode: 'DEV_MODE_PLACEHOLDER'
+  add_acess_form: 'FEATURE_FLAG_ADD_ACCESS_FORM_PLACEHOLDER',
+  dev_mode: 'DEV_MODE_PLACEHOLDER',
 }
 
 let allFeatureFlags
