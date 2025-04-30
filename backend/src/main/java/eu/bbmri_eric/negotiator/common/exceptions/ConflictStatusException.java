029673f3b13d851245919b05a14d6c648e62f99e
Milos Terzic <tmilos94@gmail.com>
2025-04-30 15:21:45 +0200

    Revert "Revert to 8ae0c0ba8c400a3938bea04173354713c831a476"
    This reverts commit 013f49d7dbe43e2402708a65b662e36a5208f4e9.

---
+++ backend/src/main/java/eu/bbmri_eric/negotiator/common/exceptions/ConflictStatusException.java
@@ -0,0 +1,7 @@
+package eu.bbmri_eric.negotiator.common.exceptions;
+
+public class ConflictStatusException extends RuntimeException {
+  public ConflictStatusException(String message) {
+    super(message);
+  }
+}
--- backend/src/main/java/eu/bbmri_eric/negotiator/common/exceptions/NegotiatorExceptionHandler.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/common/exceptions/NegotiatorExceptionHandler.java
@@ -456,6 +456,35 @@ public class NegotiatorExceptionHandler {
     return problemDetail;
   }
 
+  @ResponseStatus(HttpStatus.CONFLICT)
+  @ExceptionHandler(ConflictStatusException.class)
+  @ApiResponse(
+      responseCode = "409",
+      description = "Conflict: The status of the resource is in conflict with the one requested",
+      content =
+          @Content(
+              mediaType = "application/json+problem",
+              examples =
+                  @ExampleObject(
+                      value =
+                          """
+                                          {
+                                            "type": "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/409",
+                                            "title": "Conflict",
+                                            "status": 409,
+                                            "detail": "The status of the resource is in conflict with the one requested.",
+                                            "instance": "/api/your-endpoint"
+                                          }
+                                          """)))
+  public final ProblemDetail handleConflictStatusException(ConflictStatusException exception) {
+    ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+    problemDetail.setTitle("Conflict");
+    problemDetail.setDetail(exception.getMessage());
+    problemDetail.setType(
+        URI.create("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/409"));
+    return problemDetail;
+  }
+
   private static void extractDetails(
       MethodArgumentNotValidException e, Map<String, String> errors) {
     e.getBindingResult()
