9d23105fd4be11b640e1b6502a86dd349a2ebe76
Milos Terzic <tmilos94@gmail.com>
2025-05-09 14:46:23 +0200

    fix: styling of pdf file and removing js warnings

--- frontend/src/components/PDFButton.vue
+++ frontend/src/components/PDFButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <a
-      class="pdf-text-hover cursor-pointer"
+      class="pdf-button pdf-text-hover cursor-pointer"
       @click="createPDF"
       :style="{
         color: uiConfigurationTheme.primaryTextColor,
@@ -21,6 +21,7 @@ import bbmriLogo from '../assets/images/bbmri/nav-bar-bbmri.png'
 import eucaimLogo from '../assets/images/eucaim/home-eucaim.png'
 import canservLogo from '../assets/images/canserv/nav-bar-canserv.png'
 import { useUiConfiguration } from '../store/uiConfiguration.js'
+import { useNotificationsStore } from '../store/notifications'
 import moment from 'moment'
 import { dateFormat } from '@/config/consts'
 import { transformStatus } from '../composables/utils.js'
@@ -35,6 +36,7 @@ const props = defineProps({
 })
 
 const uiConfigurationStore = useUiConfiguration()
+const notificationsStore = useNotificationsStore()
 
 const uiConfigurationNavbar = computed(() => {
   return uiConfigurationStore.uiConfiguration?.navbar
@@ -56,78 +58,99 @@ const returnLogoSrc = computed(() => {
 })
 
 function createPDF() {
-  const pdfName = 'negotiation'
-  const doc = new jsPDF({ compress: true })
+  try {
+    const pdfName = 'negotiation'
+    const doc = new jsPDF({ compress: true })
 
-  const negotiationUser = {
-    Author: props.negotiationPdfData.author.name,
-    Email: props.negotiationPdfData.author.email,
-    'Negotiation ID': props.negotiationPdfData.id,
-    'Submitted at': moment(props.negotiationPdfData.creationDate).format(dateFormat),
-    Status: transformStatus(props.negotiationPdfData.status),
-    'Report generated at': moment().format(dateFormat),
-  }
+    const negotiationUser = {
+      Author: props.negotiationPdfData.author.name,
+      Email: props.negotiationPdfData.author.email,
+      'Negotiation ID': props.negotiationPdfData.id,
+      'Submitted at': moment(props.negotiationPdfData.creationDate).format(dateFormat),
+      Status: transformStatus(props.negotiationPdfData.status),
+      'Report generated at': moment().format(dateFormat),
+    }
 
-  doc.addImage(returnLogoSrc.value, 'JPEG', 15, 7, 50, 13, 'FAST')
+    doc.addImage(returnLogoSrc.value, 'JPEG', 15, 7, 50, 13, 'FAST')
 
-  doc.autoTable({
-    body: [['REQUEST SUMMARY']],
-    columnStyles: {
-      0: { font: 'calibri', fontStyle: 'bold', halign: 'center' },
-    },
-    startY: 25,
-    rowPageBreak: 'auto',
-    bodyStyles: { valign: 'top' },
-  })
-
-  for (const [key, value] of Object.entries(negotiationUser)) {
     doc.autoTable({
-      body: [[`${key}:`, `${value}`]],
+      body: [['REQUEST SUMMARY']],
       columnStyles: {
-        0: { cellWidth: 23, font: 'calibri', fontStyle: 'bold' },
-        1: { cellWidth: 100, font: 'calibri' },
+        0: { font: 'Times', fontStyle: 'bold', halign: 'center' },
       },
-      theme: 'plain',
-      startY: doc.lastAutoTable.finalY + 2,
-      rowPageBreak: 'auto',
-      bodyStyles: { valign: 'top' },
-    })
-  }
-
-  for (const key in props.negotiationPdfData.payload) {
-    doc.autoTable({
-      body: [[key.toUpperCase()]],
-      columnStyles: {
-        0: { font: 'calibri', fontStyle: 'bold' },
-      },
-      startY: doc.lastAutoTable.finalY + 10,
+      startY: 25,
       rowPageBreak: 'auto',
       bodyStyles: { valign: 'top' },
     })
 
-    for (const value in props.negotiationPdfData.payload[key]) {
-      if (props.negotiationPdfData.payload[key][value]) {
-        let payloadValue = props.negotiationPdfData.payload[key][value]
+    for (const [key, value] of Object.entries(negotiationUser)) {
+      doc.autoTable({
+        body: [[`${key}:`, `${value}`]],
+        columnStyles: {
+          0: { cellWidth: 23, font: 'Times', fontStyle: 'bold' },
+          1: { columnWidth: 100, halign: 'left', font: 'Times' },
+        },
+        theme: 'plain',
+        startY: doc.lastAutoTable.finalY + 2,
+        rowPageBreak: 'auto',
+        bodyStyles: { valign: 'top' },
+      })
+    }
 
-        // Check if the value is attachment and display the name if it exists
-        if (props.negotiationPdfData.payload[key][value].name)
-          payloadValue = props.negotiationPdfData.payload[key][value].name
+    for (const key in props.negotiationPdfData.payload) {
+      doc.autoTable({
+        body: [[key.toUpperCase()]],
+        columnStyles: {
+          0: { font: 'Times', fontStyle: 'bold' },
+        },
+        startY: doc.lastAutoTable.finalY + 10,
+        rowPageBreak: 'auto',
+        bodyStyles: { valign: 'top' },
+      })
 
-        doc.autoTable({
-          body: [[value + ': ', payloadValue]],
-          startY: doc.lastAutoTable.finalY + 2,
-          columnStyles: {
-            0: { cellWidth: 25, font: 'calibri', fontStyle: 'bold' },
-            1: { cellWidth: 100, font: 'calibri' },
-          },
-          theme: 'plain',
-          rowPageBreak: 'auto',
-          bodyStyles: { valign: 'top' },
-        })
+      for (const value in props.negotiationPdfData.payload[key]) {
+        if (props.negotiationPdfData.payload[key][value]) {
+          let payloadValue = props.negotiationPdfData.payload[key][value]
+
+          // Check if the value is attachment and display the name if it exists
+          if (props.negotiationPdfData.payload[key][value].name)
+            payloadValue = props.negotiationPdfData.payload[key][value].name
+
+          // transform the value to a string
+          payloadValue = formatString(payloadValue)
+
+          doc.autoTable({
+            body: [[value + ': ', payloadValue]],
+            startY: doc.lastAutoTable.finalY + 2,
+            columnStyles: {
+              0: { cellWidth: 25, font: 'Times', fontStyle: 'bold' },
+              1: { columnWidth: 100, halign: 'left', font: 'Times' },
+            },
+            theme: 'plain',
+            rowPageBreak: 'auto',
+            bodyStyles: { valign: 'top' },
+          })
+        }
       }
     }
+
+    doc
+      .save(pdfName + '.pdf', { returnPromise: true })
+      .then(() => {
+        notificationsStore.setNotification('File successfully saved', 'success')
+      })
+      .catch(() => {
+        notificationsStore.setNotification('Error saving file', 'warning')
+      })
+  } catch (error) {
+    console.log(error)
+    notificationsStore.setNotification('Error saving file', 'warning')
   }
-  doc.save(pdfName + '.pdf')
+}
+
+function formatString(str) {
+  str = str.toString().replace(/,/g, ', ')
+  return str.toString().replace(/_/g, ' ')
 }
 </script>
 
