3ee2f3b49e84ab6ef2f2e1b3b7dffbdcb711952b
Emilian Jungwirth <emilian.jung+github@gmail.com>
2025-05-22 15:09:54 +0200

    refactor: remove unused import in NegotiationPdfServiceImpl

--- backend/src/main/java/eu/bbmri_eric/negotiator/negotiation/pdf/NegotiationPdfServiceImpl.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/negotiation/pdf/NegotiationPdfServiceImpl.java
@@ -7,7 +7,6 @@ import java.io.ByteArrayOutputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
