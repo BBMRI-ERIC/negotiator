537765bc583ca5a6555fcdbc2344d977f0af50e0
Emilian Jungwirth <emilian.jung+github@gmail.com>
2025-05-23 11:32:53 +0200

    test: update PDF retrieval tests to use query parameters for template names

--- backend/src/test/java/eu/bbmri_eric/negotiator/integration/api/v3/NegotiationControllerTests.java
+++ backend/src/test/java/eu/bbmri_eric/negotiator/integration/api/v3/NegotiationControllerTests.java
@@ -1730,8 +1730,8 @@ public class NegotiationControllerTests {
   public void testGetPdf_Ok_WithTemplateNameProvided() throws Exception {
     mockMvc
         .perform(
-            MockMvcRequestBuilders.get(
-                "/v3/negotiations/negotiation-3/pdf", "pdf-negotiation-summary"))
+            MockMvcRequestBuilders.get("/v3/negotiations/negotiation-3/pdf")
+                .param("template", "pdf-negotiation-summary"))
         .andExpect(status().isOk())
         .andExpect(content().contentType("application/pdf"))
         .andExpect(
@@ -1742,10 +1742,9 @@ public class NegotiationControllerTests {
   @WithUserDetails("admin")
   public void testGetPdf_BadRequest_WithTemplateNameProvided() throws Exception {
     mockMvc
-        .perform(MockMvcRequestBuilders.get("/v3/negotiations/negotiation-3/pdf", "email-footer"))
-        .andExpect(status().isOk())
-        .andExpect(content().contentType("application/pdf"))
-        .andExpect(
-            header().string("Content-Disposition", org.hamcrest.Matchers.containsString(".pdf")));
+        .perform(
+            MockMvcRequestBuilders.get("/v3/negotiations/negotiation-3/pdf")
+                .param("template", "email-footer"))
+        .andExpect(status().isBadRequest());
   }
 }
