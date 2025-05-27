e38bae3ac7e3b28425772ebb073588b94bdc6213
Milos Terzic <tmilos94@gmail.com>
2025-05-27 15:31:23 +0200

    feat: disable download pdf button, until you get first response

--- frontend/src/components/PDFButton.vue
+++ frontend/src/components/PDFButton.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useUiConfiguration } from '../store/uiConfiguration.js'
 import { useNegotiationPageStore } from '@/store/negotiationPage.js'
 import { useNotificationsStore } from '../store/notifications'
@@ -28,33 +28,37 @@ const props = defineProps({
 const uiConfigurationStore = useUiConfiguration()
 const negotiationPageStore = useNegotiationPageStore()
 const notificationsStore = useNotificationsStore()
-
+const loadingPdf = ref(false)
 
 const uiConfigurationTheme = computed(() => {
   return uiConfigurationStore.uiConfiguration?.theme
 })
 
 async function retrievePDF() {
-  try {
-    const pdfData = await negotiationPageStore.retrieveNegotiationPDF(props.negotiationPdfData.id)
-    const pdfBlob = new Blob([pdfData], { type: 'application/pdf' })
+  if (!loadingPdf.value) {
+    loadingPdf.value = true
+    try {
+      const pdfData = await negotiationPageStore.retrieveNegotiationPDF(props.negotiationPdfData.id)
+      const pdfBlob = new Blob([pdfData], { type: 'application/pdf' })
 
-    const link = document.createElement('a')
-    link.href = URL.createObjectURL(pdfBlob)
-    link.download = `Negotiation_${props.negotiationPdfData.id}.pdf`
-    document.body.appendChild(link);
-    link.click()
-    document.body.removeChild(link);
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(pdfBlob)
+      link.download = `Negotiation_${props.negotiationPdfData.id}.pdf`
+      document.body.appendChild(link);
+      link.click()
+      document.body.removeChild(link);
 
-    URL.revokeObjectURL(link.href)
+      URL.revokeObjectURL(link.href)
 
-    notificationsStore.setNotification('File successfully saved', 'success');
-  } catch (error) {
-    console.error('Error retrieving or saving the PDF:', error)
-    notificationsStore.setNotification('Error saving file', 'warning');
+      notificationsStore.setNotification('File successfully saved', 'success');
+      loadingPdf.value = false
+    } catch (error) {
+      console.error('Error retrieving or saving the PDF:', error)
+      notificationsStore.setNotification('Error saving file', 'warning');
+      loadingPdf.value = false
+    }
   }
 }
-
 </script>
 
 <style scoped>
