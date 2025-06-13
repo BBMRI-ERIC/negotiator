c483b06e9fc1a148991d4867738244146f97bfdb
Emilian Jungwirth <emilian.jung+github@gmail.com>
2025-06-13 12:02:33 +0200

    feat: enhance PDF merging error handling and logging

--- backend/src/main/java/eu/bbmri_eric/negotiator/common/PdfMerger.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/common/PdfMerger.java
@@ -3,19 +3,36 @@ package eu.bbmri_eric.negotiator.common;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
+
+import lombok.extern.apachecommons.CommonsLog;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.Loader;
 
-
+@CommonsLog
 public class PdfMerger {
     public static byte[] mergePdfs(List<byte[]> pdfBytesList) throws IOException {
         PDDocument mergedPdf = new PDDocument();
 
         try {
             for (byte[] pdfBytes : pdfBytesList) {
-                PDDocument doc = Loader.loadPDF(pdfBytes);
-                mergeDocument(mergedPdf, doc);
+                if (pdfBytes == null || pdfBytes.length == 0) {
+                    continue;
+                }
+                PDDocument doc = new PDDocument();
+                try{
+                    doc = Loader.loadPDF(pdfBytes);
+                    mergeDocument(mergedPdf, doc);
+                } catch (IOException e) {
+                    log.error("Failed to merge PDF document: " + e.getMessage(), e);
+                    doc.addPage(new PDPage());
+                    doc.getDocumentInformation().setTitle("Error Document");
+                    doc.getDocumentInformation().setSubject("An error occurred while merging PDF documents.");
+                    mergeDocument(mergedPdf, doc);
+                } finally {
+                    doc.close();
+                }
             }
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
