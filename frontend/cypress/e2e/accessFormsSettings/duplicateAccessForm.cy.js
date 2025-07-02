11634aef35d2ea2fbdb711889499cdf06771aa83
Milos Terzic <tmilos94@gmail.com>
2025-07-02 03:17:09 +0200

    fix: comment test, code not visible on Prod, until feature flag removed

--- frontend/cypress/e2e/accessFormsSettings/duplicateAccessForm.cy.js
+++ frontend/cypress/e2e/accessFormsSettings/duplicateAccessForm.cy.js
@@ -1,32 +1,35 @@
-/// <reference types="cypress" />
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// commented because of feature flag - add_access_forms
 
-describe("Test access forms settings", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:8080")
-        cy.login("Admin", "admin")
-        cy.visit("http://localhost:8080/settings")
-    })
+// /// <reference types="cypress" />
 
-    context("check if access form settings section is visible", () => {
-        it("test if tittle is visible", () => {
-            cy.get('.access-forms-section > .mb-1').should("be.visible")
-        })
-        it("test if tittle is subtittle visible", () => {
-            cy.get('.access-forms-section > .mb-3').should("be.visible")
-        })
-    })
+// describe("Test access forms settings", () => {
+//     beforeEach(() => {
+//         cy.visit("http://localhost:8080")
+//         cy.login("Admin", "admin")
+//         cy.visit("http://localhost:8080/settings")
+//     })
 
-    context("check if you can duplicate form", () => {
-        it("click duplicate access form button", () => {
-            // Open modal
-            cy.get(':nth-child(2) > tr > .col-3 > .btn').should("be.visible")
+//     context("check if access form settings section is visible", () => {
+//         it("test if tittle is visible", () => {
+//             cy.get('.access-forms-section > .mb-1').should("be.visible")
+//         })
+//         it("test if tittle is subtittle visible", () => {
+//             cy.get('.access-forms-section > .mb-3').should("be.visible")
+//         })
+//     })
 
-            cy.get(':nth-child(2) > tr > .col-3 > .btn').click()
-        })
-        it("duplicate form is displayed", () => {
-            cy.get(':nth-child(2) > tr > .col-3 > .btn').click()
+//     context("check if you can duplicate form", () => {
+//         it("click duplicate access form button", () => {
+//             // Open modal
+//             cy.get(':nth-child(2) > tr > .col-3 > .btn').should("be.visible")
 
-            cy.get('.new-section-button > label').should("be.visible")
-        })
-    })
-})
+//             cy.get(':nth-child(2) > tr > .col-3 > .btn').click()
+//         })
+//         it("duplicate form is displayed", () => {
+//             cy.get(':nth-child(2) > tr > .col-3 > .btn').click()
+
+//             cy.get('.new-section-button > label').should("be.visible")
+//         })
+//     })
+// })
