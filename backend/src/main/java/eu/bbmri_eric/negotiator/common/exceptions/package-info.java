12139fd4f2485edf9bcd7a30f240c039e519d945
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2025-07-11 14:14:49 +0200

    refactor: remove unnecessary files

--- backend/src/main/java/eu/bbmri_eric/negotiator/common/exceptions/package-info.java
+++
@@ -1,1 +0,0 @@
-package eu.bbmri_eric.negotiator.common.exceptions;
--- backend/src/main/java/eu/bbmri_eric/negotiator/email/NotificationEmailModelAssembler.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/email/NotificationEmailModelAssembler.java
@@ -1,10 +1,14 @@
 package eu.bbmri_eric.negotiator.email;
 
+import static eu.bbmri_eric.negotiator.common.LinkBuilder.getPageLinks;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
@@ -15,7 +19,8 @@ public class NotificationEmailModelAssembler
         NotificationEmailDTO, EntityModel<NotificationEmailDTO>> {
 
   @Override
-  public EntityModel<NotificationEmailDTO> toModel(NotificationEmailDTO notificationEmailDTO) {
+  public @NotNull EntityModel<NotificationEmailDTO> toModel(
+      @NotNull NotificationEmailDTO notificationEmailDTO) {
     return EntityModel.of(notificationEmailDTO)
         .add(
             linkTo(
@@ -31,53 +36,18 @@ public class NotificationEmailModelAssembler
 
   public PagedModel<EntityModel<NotificationEmailDTO>> toPagedModel(
       Page<NotificationEmailDTO> page, NotificationEmailFilterDTO filters) {
-    PagedModel<EntityModel<NotificationEmailDTO>> pagedModel =
-        PagedModel.of(
-            page.getContent().stream().map(this::toModel).toList(),
-            new PagedModel.PageMetadata(
-                page.getSize(), page.getNumber(), page.getTotalElements(), page.getTotalPages()));
+    PagedModel.PageMetadata pageMetadata =
+        new PagedModel.PageMetadata(
+            page.getSize(), page.getNumber(), page.getTotalElements(), page.getTotalPages());
 
-    pagedModel.add(
-        linkTo(methodOn(NotificationEmailController.class).getAllNotificationEmails(filters))
-            .withSelfRel());
+    List<Link> links =
+        getPageLinks(
+            linkTo(methodOn(NotificationEmailController.class).getAllNotificationEmails(null))
+                .toUri(),
+            filters,
+            pageMetadata);
 
-    if (page.hasNext()) {
-      NotificationEmailFilterDTO nextFilters = createFiltersForPage(filters, page.getNumber() + 1);
-      pagedModel.add(
-          linkTo(methodOn(NotificationEmailController.class).getAllNotificationEmails(nextFilters))
-              .withRel("next"));
-    }
-
-    if (page.hasPrevious()) {
-      NotificationEmailFilterDTO prevFilters = createFiltersForPage(filters, page.getNumber() - 1);
-      pagedModel.add(
-          linkTo(methodOn(NotificationEmailController.class).getAllNotificationEmails(prevFilters))
-              .withRel("prev"));
-    }
-
-    NotificationEmailFilterDTO firstFilters = createFiltersForPage(filters, 0);
-    pagedModel.add(
-        linkTo(methodOn(NotificationEmailController.class).getAllNotificationEmails(firstFilters))
-            .withRel("first"));
-
-    NotificationEmailFilterDTO lastFilters =
-        createFiltersForPage(filters, page.getTotalPages() - 1);
-    pagedModel.add(
-        linkTo(methodOn(NotificationEmailController.class).getAllNotificationEmails(lastFilters))
-            .withRel("last"));
-
-    return pagedModel;
-  }
-
-  private NotificationEmailFilterDTO createFiltersForPage(
-      NotificationEmailFilterDTO originalFilters, int page) {
-    NotificationEmailFilterDTO filters = new NotificationEmailFilterDTO();
-    filters.setPage(page);
-    filters.setSize(originalFilters.getSize());
-    filters.setAddress(originalFilters.getAddress());
-    filters.setSentAfter(originalFilters.getSentAfter());
-    filters.setSentBefore(originalFilters.getSentBefore());
-    filters.setSort(originalFilters.getSort());
-    return filters;
+    return PagedModel.of(
+        page.getContent().stream().map(this::toModel).toList(), pageMetadata, links);
   }
 }
