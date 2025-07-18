790596a3537371860ebb4bc9149d6590a4d8dc50
Vittorio Meloni <vittorio.meloni@crs4.it>
2025-07-18 15:44:37 +0200

    test: fixes tests

--- backend/src/test/java/eu/bbmri_eric/negotiator/integration/service/PersonServiceImplTest.java
+++ backend/src/test/java/eu/bbmri_eric/negotiator/integration/service/PersonServiceImplTest.java
@@ -11,7 +11,6 @@ import eu.bbmri_eric.negotiator.user.UserFilterDTO;
 import eu.bbmri_eric.negotiator.user.UserResponseModel;
 import eu.bbmri_eric.negotiator.user.UserSortField;
 import eu.bbmri_eric.negotiator.util.IntegrationTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -39,29 +38,23 @@ public class PersonServiceImplTest {
   }
 
   @Test
-  @Disabled
-  void findAll_invalidSort_throwsWrongSortingPropertyException() {
-    //    assertThrows(WrongSortingPropertyException.class, () -> personService.findAll(0, 1,
-    // "invalid"));
-  }
-
-  @Test
   void findAll_pageAndSorted_ok() {
     Person person =
         personRepository.save(
             Person.builder().subjectId("test-id").name("AAAAA").email("test@test.com").build());
     UserFilterDTO filters =
         UserFilterDTO.builder()
-            .sortBy(UserSortField.name)
+            .sortBy(UserSortField.subjectId)
             .sortOrder(Sort.Direction.DESC)
             .page(0)
             .size(1)
             .build();
+
     assertEquals(
         person.getId().toString(),
         ((Page<UserResponseModel>) personService.findAllByFilters(filters))
             .getContent()
-            .get(0)
+            .getFirst()
             .getId());
   }
 
