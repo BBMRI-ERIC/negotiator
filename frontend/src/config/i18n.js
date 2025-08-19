795ef92f331a3473e78a69a23c79ee6a9c2031f2
Milos Terzic <tmilos94@gmail.com>
2025-08-19 18:20:45 +0200

    feat: add i18n for translation

---
+++ frontend/src/config/i18n.js
@@ -0,0 +1,22 @@
+export default {
+  locale: 'en-canSERV',
+  fallbackLocale: 'en',
+  messages: {
+    en: {
+      navbar: {
+        admin: 'Review requests',
+        researcher: 'Your negotiations',
+        biobanker: 'Your biobank',
+        FAQ: 'Support',
+        doc: 'Documentation',
+      },
+    },
+    'en-canSERV': {
+      navbar: {
+        admin: 'Review requests',
+        researcher: 'Service',
+        biobanker: 'Your organisation',
+      },
+    },
+  },
+}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,6 +14,8 @@ import 'vue3-tour/dist/vue3-tour.css'
 import { useOidcStore } from './store/oidc'
 import { piniaOidcCreateRouterMiddleware } from 'pinia-oidc'
 import VueDOMPurifyHTML from 'vue-dompurify-html'
+import { createI18n } from 'vue-i18n'
+import i18nConfig from './config/i18n.js'
 import('./assets/scss/theme.scss')
 
 library.add(faSpinner)
@@ -30,7 +32,9 @@ if (matomo.matomoHost !== 'MATOMO_HOST_PLACEHOLDER') {
     siteId: matomo.matomoId,
   })
 }
+const i18n = createI18n(i18nConfig)
 
+app.use(i18n)
 app.use(router)
 app.use(pinia)
 app.use(Vue3Tour)
