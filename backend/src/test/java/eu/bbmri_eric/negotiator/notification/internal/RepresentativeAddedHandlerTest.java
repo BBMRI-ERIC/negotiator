f513c576fc68e95fb06ab035adc1ce8c6a11c1b1
Chrisi <christopher.sailer@medunigraz.at>
2025-08-19 13:02:10 +0200

    refactor: changed tests to exclude logger testing

--- backend/src/test/java/eu/bbmri_eric/negotiator/notification/internal/RepresentativeAddedHandlerTest.java
+++ backend/src/test/java/eu/bbmri_eric/negotiator/notification/internal/RepresentativeAddedHandlerTest.java
@@ -4,10 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
 import eu.bbmri_eric.negotiator.governance.resource.Resource;
 import eu.bbmri_eric.negotiator.governance.resource.ResourceRepository;
 import eu.bbmri_eric.negotiator.notification.NotificationCreateDTO;
@@ -15,14 +11,12 @@ import eu.bbmri_eric.negotiator.notification.NotificationService;
 import eu.bbmri_eric.negotiator.user.Person;
 import eu.bbmri_eric.negotiator.user.PersonRepository;
 import eu.bbmri_eric.negotiator.user.RepresentativeAddedEvent;
-import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.LoggerFactory;
 
 @ExtendWith(MockitoExtension.class)
 public class RepresentativeAddedHandlerTest {
@@ -34,17 +28,11 @@ public class RepresentativeAddedHandlerTest {
   private RepresentativeAddedHandler handler;
   private Long resourceId = 1L;
   private Long representativeId = 1L;
-  private ListAppender<ILoggingEvent> listAppender;
 
   @BeforeEach
   void setUp() {
     handler =
         new RepresentativeAddedHandler(notificationService, personRepository, resourceRepository);
-
-    Logger logger = (Logger) LoggerFactory.getLogger(RepresentativeAddedHandler.class);
-    listAppender = new ListAppender<>();
-    listAppender.start();
-    logger.addAppender(listAppender);
   }
 
   @Test
@@ -70,13 +58,6 @@ public class RepresentativeAddedHandlerTest {
     handler.notify(event);
 
     verify(notificationService).createNotifications(any(NotificationCreateDTO.class));
-
-    List<ILoggingEvent> logsList = listAppender.list;
-    assertEquals(1, logsList.size());
-    assertEquals(Level.INFO, logsList.get(0).getLevel());
-    assertEquals(
-        "Notified John Doe about being assigned as a representative for resource: resource-123",
-        logsList.get(0).getFormattedMessage());
   }
 
   @Test
@@ -92,13 +73,6 @@ public class RepresentativeAddedHandlerTest {
     handler.notify(event);
 
     verify(notificationService, never()).createNotifications(any(NotificationCreateDTO.class));
-
-    List<ILoggingEvent> logsList = listAppender.list;
-    assertEquals(1, logsList.size());
-    assertEquals(Level.WARN, logsList.get(0).getLevel());
-    assertEquals(
-        "Failed to notify representative since representative not found.",
-        logsList.get(0).getFormattedMessage());
   }
 
   @Test
@@ -114,13 +88,6 @@ public class RepresentativeAddedHandlerTest {
     handler.notify(event);
 
     verify(notificationService, never()).createNotifications(any(NotificationCreateDTO.class));
-
-    List<ILoggingEvent> logsList = listAppender.list;
-    assertEquals(1, logsList.size());
-    assertEquals(Level.WARN, logsList.get(0).getLevel());
-    assertEquals(
-        "Failed to notify representative since resource not found.",
-        logsList.get(0).getFormattedMessage());
   }
 
   @Test
@@ -134,12 +101,5 @@ public class RepresentativeAddedHandlerTest {
     handler.notify(event);
 
     verify(notificationService, never()).createNotifications(any(NotificationCreateDTO.class));
-
-    List<ILoggingEvent> logsList = listAppender.list;
-    assertEquals(1, logsList.size());
-    assertEquals(Level.WARN, logsList.get(0).getLevel());
-    assertEquals(
-        "Failed to notify representative since representative and resource not found.",
-        logsList.get(0).getFormattedMessage());
   }
 }
