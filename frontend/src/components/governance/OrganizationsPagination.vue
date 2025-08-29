a9b2838362bdb9ad41ae28bf2ce8023764002652
Milos Terzic <tmilos94@gmail.com>
2025-08-29 12:49:37 +0200

    fix: format and change pageSize input

--- frontend/src/components/governance/OrganizationsPagination.vue
+++ frontend/src/components/governance/OrganizationsPagination.vue
@@ -1,60 +1,80 @@
 <template>
-  <div v-if="totalElements > 0" class="pagination">
-    <button
-      @click="$emit('previousPage')"
-      :disabled="pageNumber === 0 || loading"
-      class="page-button"
-    >
-      ‹ Prev
-    </button>
-    <span class="page-info">Page {{ pageNumber + 1 }} of {{ totalPages }}</span>
-    <button
-      @click="$emit('nextPage')"
-      :disabled="pageNumber === totalPages - 1 || loading"
-      class="page-button"
-    >
-      Next ›
-    </button>
-    <input
-      :model-value="pageSize"
-      @change="handlePageSizeChange"
-      type="number"
-      min="1"
-      max="100"
-      class="page-size-input"
-      placeholder="Page size"
-    />
+  <div v-if="totalElements > 0" class="pagination d-flex justify-content-between">
+    <div class="div-placeholder col-md-4"></div>
+    <div class="pagination-page col-md-4">
+      <button
+        @click="$emit('previousPage')"
+        :disabled="pageNumber === 0 || loading"
+        class="page-button"
+      >
+        ‹ Prev
+      </button>
+      <span class="page-info">Page {{ pageNumber + 1 }} of {{ totalPages }}</span>
+      <button
+        @click="$emit('nextPage')"
+        :disabled="pageNumber === totalPages - 1 || loading"
+        class="page-button"
+      >
+        Next ›
+      </button>
+    </div>
+    <div class="increment-decrement-input col-md-4 d-flex justify-content-end">
+      <button class="button-size-input btn btn-outline-secondary me-1" @click="changePageSize(-1)">
+        -
+      </button>
+      <input
+        :model-value="pageSize"
+        v-on:input="handlePageSizeChange"
+        :placeholder="'Page size ' + pageSize"
+        type="number"
+        min="1"
+        max="100"
+        class="page-size-input"
+      />
+
+      <button class="button-size-input btn btn-outline-secondary ms-1" @click="changePageSize(1)">
+        +
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   pageNumber: {
     type: Number,
-    required: true
+    required: true,
   },
   totalPages: {
     type: Number,
-    required: true
+    required: true,
   },
   totalElements: {
     type: Number,
-    required: true
+    required: true,
   },
   pageSize: {
     type: Number,
-    required: true
+    required: true,
   },
   loading: {
     type: Boolean,
-    required: true
-  }
+    required: true,
+  },
 })
 
 const emit = defineEmits(['previousPage', 'nextPage', 'updatePageSize'])
 
 const handlePageSizeChange = (event) => {
+  console.log(event.target.value)
   emit('updatePageSize', Number(event.target.value))
+}
+
+function changePageSize(valueChange) {
+  if (props.pageSize + valueChange > 1 || props.pageSize + valueChange < 100) {
+    const newPageSize = Math.min(Math.max(1, props.pageSize + valueChange), 100)
+    emit('updatePageSize', Number(newPageSize))
+  }
 }
 </script>
 
@@ -91,13 +111,22 @@ const handlePageSizeChange = (event) => {
   font-size: 0.95rem;
   color: #6c757d;
 }
-
+.button-size-input {
+  border: 1px solid #e8ecef;
+}
 .page-size-input {
   padding: 0.5rem;
-  margin-left: 1rem;
-  width: 80px;
+  width: 110px;
   border: 1px solid #e8ecef;
   border-radius: 0.375rem;
   font-size: 0.9rem;
+}
+
+input[type='number']::-webkit-inner-spin-button,
+input[type='number']::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  margin: 0;
 }
 </style>
