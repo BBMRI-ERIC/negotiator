95084d5ca6c33c6d3daec33b4eea61a6405f1a8a
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2025-09-11 16:50:36 +0200

    fix: HTML template sanitization

--- backend/src/main/java/eu/bbmri_eric/negotiator/template/TemplateServiceImpl.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/template/TemplateServiceImpl.java
@@ -5,8 +5,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.apachecommons.CommonsLog;
-import org.owasp.html.HtmlPolicyBuilder;
-import org.owasp.html.PolicyFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -106,41 +104,16 @@ public class TemplateServiceImpl implements TemplateService {
   }
 
   private String sanitizeHtml(String html) {
-    PolicyFactory policy =
-        new HtmlPolicyBuilder()
-            .allowElements(
-                "html", "head", "body", "title", "meta", "style", "p", "br", "hr", "div", "span",
-                "h1", "h2", "h3", "h4", "h5", "h6", "strong", "b", "em", "i", "u", "s", "small",
-                "mark", "ul", "ol", "li", "dl", "dt", "dd", "table", "thead", "tbody", "tfoot",
-                "tr", "td", "th", "caption", "a", "img")
-            .allowAttributes("href")
-            .onElements("a")
-            .allowAttributes("src", "alt", "title", "width", "height")
-            .onElements("img")
-            .allowAttributes("class", "id", "style")
-            .globally()
-            .allowAttributes(
-                "th:text",
-                "th:utext",
-                "th:if",
-                "th:unless",
-                "th:each",
-                "th:with",
-                "th:object",
-                "th:field",
-                "th:value",
-                "th:href",
-                "th:src",
-                "th:alt",
-                "th:title",
-                "th:class",
-                "th:style")
-            .globally()
-            .toFactory();
-    String sanitized = policy.sanitize(html);
-    if (!html.equals(sanitized)) {
-      log.warn("Template content was sanitized - potentially unsafe content removed");
+    String originalHtml = html;
+    html = html.replaceAll("(?is)<(script|object|embed|applet|iframe|form)[^>]*>.*?</\\1>", "");
+    html = html.replaceAll("(?i)<(script|object|embed|applet|iframe|form)[^>]*/?>", "");
+    html = html.replaceAll("(?i)javascript:", "");
+    html = html.replaceAll("(?i)\\s+on\\w+\\s*=\\s*(['\"]).*?\\1", "");
+    if (!originalHtml.equals(html)) {
+      log.debug(
+          "Template content sanitized - removed script-like content and dangerous attributes");
     }
-    return sanitized;
+
+    return html;
   }
 }
