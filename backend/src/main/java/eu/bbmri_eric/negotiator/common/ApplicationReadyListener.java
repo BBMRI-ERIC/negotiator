e1e61f4067579844f92d930bb526234d16faee83
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2025-10-01 17:12:40 +0200

    feat: add app startup message

---
+++ backend/src/main/java/eu/bbmri_eric/negotiator/common/ApplicationReadyListener.java
@@ -0,0 +1,30 @@
+package eu.bbmri_eric.negotiator.common;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/** Start up message printer */
+@Component
+public class ApplicationReadyListener implements ApplicationListener<ApplicationReadyEvent> {
+  public static final String ANSI_GREEN = "\u001B[32m";
+  public static final String ANSI_RESET = "\u001B[0m";
+
+  @Override
+  public void onApplicationEvent(ApplicationReadyEvent event) {
+    List<String> messages =
+        new ArrayList<>(
+            List.of(
+                "🛩️ Red Five standing by. All systems go.",
+                "🚀 Hyperdrive engaged. Punch it, Chewie!",
+                "🎉 App started. This is where the fun begins.",
+                "✨ The Force is with us. App startup complete.",
+                "🌠 App ready. Preparing to jump to hyperspace."));
+    Collections.shuffle(messages);
+    System.out.println(ANSI_GREEN + messages.getFirst() + ANSI_RESET);
+  }
+}
