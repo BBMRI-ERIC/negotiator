572c98ba411ac9a18992b182b726f06b1e0cc83d
Milos Terzic <tmilos94@gmail.com>
2025-10-06 17:36:45 +0200

    fix: Uncaught TypeError: Cannot destructure property 'state' of 'options' as it is undefined.

--- frontend/src/store/oidc.js
+++ frontend/src/store/oidc.js
@@ -2,10 +2,12 @@ import { defineStore } from 'pinia'
 import { piniaOidcCreateStoreModule } from 'pinia-oidc'
 import oidcSettings from '../config/oidc'
 
-export const useOidcStore = defineStore(
-  piniaOidcCreateStoreModule(
-    oidcSettings,
-    // Optional OIDC store settings
-    { removeUserWhenTokensExpire: false },
-  ),
+// Create the OIDC store module
+const oidcStoreModule = piniaOidcCreateStoreModule(
+  oidcSettings,
+  // Optional OIDC store settings
+  { removeUserWhenTokensExpire: false },
 )
+
+// Define the Pinia store using the OIDC store module
+export const useOidcStore = defineStore('oidc', oidcStoreModule)
