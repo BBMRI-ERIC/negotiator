a01319a1da17cd4539060901cfea52cc91f40c5f
Milos Terzic <tmilos94@gmail.com>
2025-10-25 01:10:20 +0200

    feat: Refactored Vue tour into separate components, fixed role-based rendering, and added new menu tour steps

---
+++ frontend/src/components/vue-tours/VueTourDefault.vue
@@ -0,0 +1,43 @@
+<template>
+  <VueTour :tourName="'defoultTour'" :steps="steps" :isStoreToLocalStorage="true" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import VueTour from '../vue-tours/VueTour.vue'
+
+const steps = ref([
+  {
+    target: '#v-step-0',
+    header: {
+      title: 'Welcome',
+    },
+    content:
+      'In the <strong>Negotiator</strong>, you can view the status of your negotiations and stay in contact with the providers of the desired resources.',
+  },
+  {
+    target: '#v-step-1',
+    header: {
+      title: 'Selection of the view.',
+    },
+    content: 'You can present your enquiries in a compact table or an informative card layout.',
+  },
+  {
+    target: '#v-step-2',
+    header: {
+      title: 'Status',
+    },
+    params: {
+      placement: 'top',
+    },
+    content: 'You can see the current status of your enquiry at a glance.',
+  },
+  {
+    target: '#v-step-3',
+    header: {
+      title: 'Filter',
+    },
+    content: 'You also have the option of sorting and filtering your negotiations.',
+  },
+])
+</script>
