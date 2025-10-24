feed247b60dec18eebc29cb59033bab39f45ebd0
Milos Terzic <tmilos94@gmail.com>
2025-10-25 01:10:20 +0200

    feat: Refactored Vue tour into separate components, fixed role-based rendering, and added new menu tour steps

---
+++ frontend/src/components/vue-tours/VueTour.vue
@@ -0,0 +1,125 @@
+<template>
+  <v-tour name="myTour" :steps="steps" :callbacks="myCallbacks" :options="{ debug: true }" />
+</template>
+
+<script>
+export default {
+  mounted: function () {
+    this.$tours.myTour.start()
+  },
+}
+</script>
+
+<script setup>
+const props = defineProps({
+  tourName: {
+    type: String,
+    required: true,
+    default: 'defaultTour',
+  },
+  steps: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+  isStoreToLocalStorage: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+})
+
+const myCallbacks = {
+  onSkip: () => {
+    if (props.isStoreToLocalStorage) {
+      localStorage.setItem(props.tourName, true)
+    }
+  },
+  onFinish: () => {
+    if (props.isStoreToLocalStorage) {
+      localStorage.setItem(props.tourName, true)
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.v-step {
+  min-width: 420px !important;
+  padding: 20px !important;
+  .v-step__header {
+    font-size: 24px;
+    font-weight: bold;
+  }
+}
+/* step-1 */
+.v-step[id='v-step-46f34eda'] {
+  background-color: #63e6be !important;
+  color: #183153 !important;
+
+  .v-step__arrow,
+  .v-step__arrow--dark:before {
+    background-color: #63e6be !important;
+  }
+  .v-step__header {
+    background-color: #63e6be !important;
+  }
+  .v-step__button {
+    color: #183153 !important;
+    border: 0.05rem solid #183153 !important;
+  }
+}
+/* step-2 */
+.v-step[id='v-step-46f34ed8'] {
+  background-color: #74c0fc !important;
+  color: #183153 !important;
+
+  .v-step__arrow,
+  .v-step__arrow:before {
+    background-color: #74c0fc !important;
+  }
+  .v-step__header {
+    background-color: #74c0fc !important;
+  }
+  .v-step__button {
+    color: #183153 !important;
+    border: 0.05rem solid #183153 !important;
+  }
+}
+
+/* step-3 */
+.v-step[id='v-step-46f34ed6'] {
+  background-color: #ffd43b !important;
+  color: #183153 !important;
+
+  .v-step__arrow,
+  .v-step__arrow:before {
+    background-color: #ffd43b !important;
+  }
+  .v-step__header {
+    background-color: #ffd43b !important;
+  }
+  .v-step__button {
+    color: #183153 !important;
+    border: 0.05rem solid #183153 !important;
+  }
+}
+
+/* step-4 */
+.v-step[id='v-step-46f34ed4'] {
+  background-color: #63e6be !important;
+  color: #183153 !important;
+
+  .v-step__arrow,
+  .v-step__arrow--dark:before {
+    background-color: #63e6be !important;
+  }
+  .v-step__header {
+    background-color: #63e6be !important;
+  }
+  .v-step__button {
+    color: #183153 !important;
+    border: 0.05rem solid #183153 !important;
+  }
+}
+</style>
