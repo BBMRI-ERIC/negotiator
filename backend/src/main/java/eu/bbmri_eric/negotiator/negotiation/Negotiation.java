0dbed5171224052bea5552f97d84e806e345c4ae
RadovanTomik <radovan.tomasik@bbmri-eric.eu>
2026-01-21 15:42:31 +0100

    fix: add resource removal

--- backend/src/main/java/eu/bbmri_eric/negotiator/negotiation/Negotiation.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/negotiation/Negotiation.java
@@ -80,7 +80,8 @@ public class Negotiation extends AuditEntity {
 
   @OneToMany(
       mappedBy = "id.negotiation",
-      cascade = {CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.MERGE})
+      cascade = {CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.MERGE},
+      orphanRemoval = true)
   @NotNull
   @Builder.Default
   @Getter(AccessLevel.PRIVATE)
@@ -209,6 +210,16 @@ public class Negotiation extends AuditEntity {
    */
   public boolean addResource(Resource resource) {
     return this.resourcesLink.add(new NegotiationResourceLink(this, resource, null));
+  }
+
+  /**
+   * Remove a resource from the Negotiation.
+   *
+   * @param resource to be removed.
+   * @return true if the resource was removed, false otherwise
+   */
+  public boolean removeResource(Resource resource) {
+    return this.resourcesLink.removeIf(link -> link.getResource().getId().equals(resource.getId()));
   }
 
   private void buildResourceStateChangeRecord(Resource resource, NegotiationResourceState state) {
