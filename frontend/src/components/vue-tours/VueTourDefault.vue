2c24d4c965d712be3494ef5e8ce18f7a3907aaf9
Milos Terzic <tmilos94@gmail.com>
2026-02-02 12:25:49 +0100

    fix: refactor duplicated lines

--- frontend/src/components/vue-tours/VueTourDefault.vue
+++ frontend/src/components/vue-tours/VueTourDefault.vue
@@ -1,36 +1,10 @@
 <template>
-  <VueTour :tourName="'vue-tour-default-1'" :steps="steps" :isStoreToLocalStorage="true" />
+  <VueTour :tourName="'vue_default_tour_1'" :steps="steps" :isStoreToLocalStorage="true" />
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { useTourSteps } from '@/composables/useTourSteps'
 import VueTour from '../vue-tours/VueTour.vue'
 
-const steps = ref([
-  {
-    target: '#v-step-0',
-    header: {
-      title: 'Welcome',
-    },
-    content:
-      'In the <strong>Negotiator</strong>, you can view the status of your negotiations and stay in contact with the providers of the desired resources.',
-  },
-  {
-    target: '#v-step-2',
-    header: {
-      title: 'Status',
-    },
-    params: {
-      placement: 'top',
-    },
-    content: 'You can see the current status of your enquiry at a glance.',
-  },
-  {
-    target: '.v-step-3',
-    header: {
-      title: 'Filter',
-    },
-    content: 'You also have the option of sorting and filtering your negotiations.',
-  },
-])
+const steps = useTourSteps('vue_default_tour_1')
 </script>
--- frontend/src/composables/useTourSteps.js
+++ frontend/src/composables/useTourSteps.js
@@ -1,7 +1,150 @@
 import { ref } from 'vue'
+import { useVueTourStore } from '@/store/vueTour.js'
 
 export function useTourSteps(tourName) {
+  const vueTourStore = useVueTourStore()
+
   const tourSteps = {
+    vue_default_tour_1: [
+      {
+        target: '#v-step-0',
+        header: {
+          title: 'Welcome',
+        },
+        content:
+          'In the <strong>Negotiator</strong>, you can view the status of your negotiations and stay in contact with the providers of the desired resources.',
+      },
+      {
+        target: '#v-step-2',
+        header: {
+          title: 'Status',
+        },
+        params: {
+          placement: 'top',
+        },
+        content: 'You can see the current status of your enquiry at a glance.',
+      },
+      {
+        target: '.v-step-3',
+        header: {
+          title: 'Filter',
+        },
+        content: 'You also have the option of sorting and filtering your negotiations.',
+      },
+    ],
+    negotiationTour: [
+      {
+        target: '#v-step-0',
+        header: {
+          title: 'Negotiation tour',
+        },
+        content:
+          'Click on a negotiation from the list to continue the tour. This tour will guide you through the key features of the negotiation view.',
+      },
+      {
+        target: '.v-step-negotiation-0',
+        header: {
+          title: 'Negotiation title',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'This is the title of the negotiation you have selected. It provides a brief overview of the negotiation topic.',
+        before: () =>
+          new Promise((resolve) => {
+            if (vueTourStore.isNegotiationVisible) {
+              resolve('foo')
+            }
+          }),
+      },
+      {
+        target: '.v-step-negotiation-1',
+        header: {
+          title: 'Negotiation body',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'Here you can view the main content of the negotiation, including all relevant details and information.',
+      },
+      {
+        target: '.v-step-negotiation-3',
+        header: {
+          title: 'Negotiation author',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'This section displays information about the author of the negotiation, including their name and contact details.',
+      },
+      {
+        target: '.v-step-negotiation-4',
+        header: {
+          title: 'Negotiation id',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'The unique identifier for this negotiation. It is used for tracking and reference purposes.',
+      },
+      {
+        target: '.v-step-negotiation-5',
+        header: {
+          title: 'Negotiation submission date',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content: 'This indicates when the negotiation was initially submitted.',
+      },
+      {
+        target: '.v-step-negotiation-6',
+        header: {
+          title: 'Negotiation status',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'This shows the current status of the negotiation, such as Draft, Submitted, or Closed.',
+      },
+      {
+        target: '.v-step-negotiation-7',
+        header: {
+          title: 'Manage negotiation actions',
+        },
+        params: {
+          placement: 'bottom',
+          enableScrolling: false,
+        },
+        content:
+          'From here, you can manage various actions related to the negotiation, such as updating its status or deleting it if applicable.',
+      },
+      {
+        target: '.v-step-negotiation-8',
+        header: {
+          title: 'Download negotiation data',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'You can download all relevant data associated with this negotiation for offline review or record-keeping purposes.',
+      },
+      {
+        target: '.v-step-negotiation-9',
+        header: {
+          title: 'Messaging panel',
+        },
+
+        content:
+          'This panel allows you to communicate with other parties involved in the negotiation. You can send and receive messages here.',
+      },
+    ],
     filterSortTour: [
       {
         target: '.v-step-20',
@@ -43,6 +186,141 @@ export function useTourSteps(tourName) {
         params: { enableScrolling: false },
         content:
           'Click the "Clear all filters" button to reset all applied filters and view the complete list of negotiations again.',
+      },
+    ],
+    governanceTour: [
+      {
+        target: '.v-step-governance-0',
+        header: {
+          title: 'Organizations',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'Menage and view details about the organizations and resources you are affiliated with in the Governance section.',
+      },
+      {
+        target: '.v-step-governance-1',
+        header: {
+          title: 'Networks Management',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content: 'Manage networks and their connections to resources.',
+      },
+      {
+        target: '.v-step-governance-2',
+        header: {
+          title: 'Organization search',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'Here you can search for specific organizations within the governance section by entering relevant keywords or names.',
+      },
+      {
+        target: '.v-step-governance-3',
+        header: {
+          title: 'Create new organization',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'Click this button to initiate the process of creating a new organization within the governance framework.',
+      },
+    ],
+    adminSettingsTour: [
+      {
+        target: '.v-step-settings-0',
+        header: {
+          title: 'Administrator Console',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'The Admin Settings Page is a centralized interface for administrators to configure and manage system-wide settings. This page provides tools to customize application behavior, manage user roles, and ensure the system operates according to organizational requirements.',
+      },
+      {
+        target: '.v-step-settings-1',
+        header: {
+          title: 'Information Requirements',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'The Negotiator allows you to define additional information requirements for Resource State Machine transitions available to the Representatives.',
+      },
+      {
+        target: '.v-step-settings-2',
+        header: {
+          title: 'Webhooks',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'The Negotiator uses a webhook system to allow external systems to be notified of events that occur within the Negotiator. This guide provides an overview of how to set up and use webhooks with the Negotiator.',
+      },
+      {
+        target: '.v-step-settings-3',
+        header: {
+          title: 'Emails',
+        },
+        params: {
+          enableScrolling: false,
+        },
+        content:
+          'Search and manage all email templates used by the Negotiator for various notifications and communications with users.',
+      },
+      {
+        target: '.v-step-settings-4',
+        header: {
+          title: 'Users',
+        },
+        params: {
+          enableScrolling: true,
+        },
+        content:
+          'Search user accounts, roles, and permissions within the Negotiator system to ensure proper access control and security.',
+      },
+      {
+        target: '.v-step-settings-5',
+        header: {
+          title: 'Templates',
+        },
+        params: {
+          enableScrolling: true,
+        },
+        content:
+          'The Negotiator provides a set of customizable templates used for generating PDFs, emails, and more',
+      },
+      {
+        target: '.v-step-settings-6',
+        header: {
+          title: 'Access Forms',
+        },
+        params: {
+          enableScrolling: true,
+        },
+        content:
+          'Adding and managing access forms that users must complete to request access to resources within the Negotiator platform.',
+      },
+      {
+        target: '.v-step-settings-7',
+        header: {
+          title: 'Form Elements',
+        },
+        params: {
+          enableScrolling: true,
+        },
+        content:
+          'Create and customize form elements that can be used in access forms throughout the Negotiator system.',
       },
     ],
   }
