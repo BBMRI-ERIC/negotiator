56eaa5cce5d1aebc2798ac21e419a2e3de14ffe6
Markus Stetschnig <markus.stetschnig@bbmri-eric.eu>
2026-03-09 17:04:46 +0100

    fix: implement silent token renew callback

--- frontend/start.sh
+++ frontend/start.sh
@@ -11,6 +11,7 @@ for file in assets/index*.js;
          -e "s|REDIRECT_URI_PLACEHOLDER|${REDIRECT_URI:-default_redirect_uri}|g" \
          -e "s|SCOPES_PLACEHOLDER|${SCOPES:-default_scopes}|g" \
          -e "s|LOGOUT_URI_PLACEHOLDER|${LOGOUT_URI:-default_logout_uri}|g" \
+         -e "s|SILENT_REDIRECT_URI_PLACEHOLDER|${SILENT_REDIRECT_URI:-default_silent_redirect_uri}|g" \
          -e "s|RESOURCES_PLACEHOLDER|${API_RESOURCES:-default_resources}|g" \
          -e "s|MATOMO_HOST_PLACEHOLDER|${MATOMO_HOST:-default_matomo_host}|g" \
          -e "s|MATOMO_SITE_ID_PLACEHOLDER|${MATOMO_SITE_ID:-default_site_id}|g" \
