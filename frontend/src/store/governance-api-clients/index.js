893ec89d01c7d964bb3d76ecfbabd9feb1761e76
Vittorio Meloni <vittorio.meloni@crs4.it>
2026-03-26 16:07:13 +0100

    refactor: perunClient add group hierarchy and mappings

--- frontend/src/store/governance-api-clients/index.js
+++ frontend/src/store/governance-api-clients/index.js
@@ -2,14 +2,21 @@ import { NegotiatorClient } from './negotiatorClient'
 import { PerunClient } from './perunClient'
 
 export class GovernanceServiceFactory {
+  client = null
+
   static getClient() {
-    switch (import.meta.env.VITE_GOVERNANCE_CLIENT) {
-      case 'NEGOTIATOR':
-        return new NegotiatorClient()
-      case 'PERUN':
-        return new PerunClient()
-      default:
-        throw new Error(`Unknown provider type: ${import.meta.env.VITE_GOVERNANCE_CLIENT}`)
+    if (this.client == null) {
+      switch (import.meta.env.VITE_GOVERNANCE_CLIENT) {
+        case 'NEGOTIATOR':
+          this.client = new NegotiatorClient()
+          break
+        case 'PERUN':
+          this.client = new PerunClient()
+          break
+        default:
+          throw new Error(`Unknown provider type: ${import.meta.env.VITE_GOVERNANCE_CLIENT}`)
+      }
     }
+    return this.client
   }
 }
