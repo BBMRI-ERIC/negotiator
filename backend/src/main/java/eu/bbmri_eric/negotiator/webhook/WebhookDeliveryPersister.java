f44252439f2064254314459aab3cd32ed73c4166
Markus Stetschnig <markus.stetschnig@bbmri-eric.eu>
2026-03-26 13:37:39 +0100

    refactor: change transaction boundry

---
+++ backend/src/main/java/eu/bbmri_eric/negotiator/webhook/WebhookDeliveryPersister.java
@@ -0,0 +1,30 @@
+package eu.bbmri_eric.negotiator.webhook;
+
+import eu.bbmri_eric.negotiator.common.exceptions.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+class WebhookDeliveryPersister {
+
+  private final WebhookRepository webhookRepository;
+  private final ModelMapper modelMapper;
+
+  WebhookDeliveryPersister(WebhookRepository webhookRepository, ModelMapper modelMapper) {
+    this.webhookRepository = webhookRepository;
+    this.modelMapper = modelMapper;
+  }
+
+  @Transactional
+  DeliveryDTO persist(Long webhookId, Delivery delivery) {
+    Webhook webhook =
+        webhookRepository
+            .findById(webhookId)
+            .orElseThrow(
+                () -> new EntityNotFoundException("Webhook not found with id: " + webhookId));
+    webhook.addDelivery(delivery);
+    webhook = webhookRepository.saveAndFlush(webhook);
+    return modelMapper.map(webhook.getDeliveries().get(0), DeliveryDTO.class);
+  }
+}
