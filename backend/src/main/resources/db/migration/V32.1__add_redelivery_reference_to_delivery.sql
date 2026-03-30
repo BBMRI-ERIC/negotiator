b7611da991e0872e6280e51324f5bb2ac05f46fb
Markus Stetschnig <markus.stetschnig@bbmri-eric.eu>
2026-03-30 14:08:08 +0200

    fix: remove foreign key on column field

--- backend/src/main/resources/db/migration/V32.1__add_redelivery_reference_to_delivery.sql
+++ backend/src/main/resources/db/migration/V32.1__add_redelivery_reference_to_delivery.sql
@@ -1,11 +1,5 @@
 ALTER TABLE delivery
-ADD COLUMN redelivery_of_delivery_id VARCHAR(36);
+ADD COLUMN IF NOT EXISTS redelivery_of_delivery_id VARCHAR(36);
 
-ALTER TABLE delivery
-ADD CONSTRAINT fk_delivery_redelivery_of
-FOREIGN KEY (redelivery_of_delivery_id)
-REFERENCES delivery (id)
-ON DELETE SET NULL;
-
-CREATE INDEX idx_delivery_redelivery_of
+CREATE INDEX IF NOT EXISTS idx_delivery_redelivery_of
 ON delivery (redelivery_of_delivery_id);
