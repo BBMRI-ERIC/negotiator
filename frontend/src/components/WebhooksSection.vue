3224c41f83ffc38282ac055d056b68e3415726d5
Markus Stetschnig <markus.stetschnig@bbmri-eric.eu>
2026-04-20 10:23:28 +0200

    feat: remove webhook description on admin page

--- frontend/src/components/WebhooksSection.vue
+++ frontend/src/components/WebhooksSection.vue
@@ -6,15 +6,5 @@
         >Manage Webhooks</RouterLink
       >
     </div>
-    <div class="text-muted mb-3">
-      Webhooks allow external services to be notified when certain events happen. When the specified
-      events occur, we send a POST request to each URL provided. Learn more in our
-      <a
-        href="https://bbmri-eric.github.io/negotiator/administrator#webhooks"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Webhooks Documentation</a
-      >.
-    </div>
   </div>
 </template>
