d16870107f7d1b93e4f06f8ca940b051365bd88e
Markus Stetschnig <markus.stetschnig@bbmri-eric.eu>
2026-05-26 19:30:15 +0200

    feat: adjust the admin page header component

--- frontend/src/components/AdminSettingsPageHeader.vue
+++ frontend/src/components/AdminSettingsPageHeader.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="admin-settings-page-header mb-2">
-    <div class="d-flex justify-content-between align-items-center mb-1">
+  <div class="admin-settings-page-header mb-3">
+    <div class="d-flex justify-content-between align-items-center mb-2">
       <h2 class="text-left mb-0">{{ title }}</h2>
-      <div class="header-actions">
-        <slot name="actions" />
+      <div v-if="actions" class="d-flex gap-2">
+        <slot class="test" name="actions" />
       </div>
     </div>
-    <hr class="my-2" />
 
-    <div v-if="hasDescription" class="text-muted mb-3">
+    <div v-if="description" class="text-muted">
       <slot name="description" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { computed, useSlots } from 'vue'
+import { useSlots } from 'vue'
 
 defineProps({
   title: {
@@ -24,6 +23,5 @@ defineProps({
   },
 })
 
-const slots = useSlots()
-const hasDescription = computed(() => Boolean(slots.description))
+const { actions, description } = useSlots()
 </script>
