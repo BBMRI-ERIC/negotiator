a883bd10d99b10ce2aec884083b9ff13b07e30b5
Ondrej Holis <ondrej.holis@sap.com>
2026-08-14 07:51:22 +0200

    fix: revert changes

--- frontend/src/middlewares/hasUser.js
+++ frontend/src/middlewares/hasUser.js
@@ -2,7 +2,9 @@
 // call retrieveUser only once because otherwise we get an error on backend
 export default async function hasUser({ next, userStore }) {
   if (Object.keys(userStore.userInfo).length === 0) {
-    await userStore.retrieveUser()
+    await userStore.retrieveUser().then(() => {
+      return next()
+    })
   }
   return next()
 }
