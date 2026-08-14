55da820b4d8736acccef390d53019d122345e712
Chrisi <christopher.sailer@medunigraz.at>
2026-08-14 14:49:19 +0200

    refactor: formatted backend code

--- backend/src/main/java/eu/bbmri_eric/negotiator/governance/organization/OrganizationForNegotiationDTO.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/governance/organization/OrganizationForNegotiationDTO.java
@@ -2,7 +2,6 @@ package eu.bbmri_eric.negotiator.governance.organization;
 
 import eu.bbmri_eric.negotiator.negotiation.state_machine.resource.NegotiationResourceState;
 import io.swagger.v3.oas.annotations.media.Schema;
-
 import lombok.*;
 import org.springframework.hateoas.server.core.Relation;
 
