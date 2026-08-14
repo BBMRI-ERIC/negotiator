e6e67f5d40eb8b8d2e8be4f868c3757fd0273cf0
Chrisi <christopher.sailer@medunigraz.at>
2026-08-14 14:41:44 +0200

    refactor: introduced lombok data annotation

--- backend/src/main/java/eu/bbmri_eric/negotiator/governance/organization/OrganizationForNegotiationDTO.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/governance/organization/OrganizationForNegotiationDTO.java
@@ -2,34 +2,17 @@ package eu.bbmri_eric.negotiator.governance.organization;
 
 import eu.bbmri_eric.negotiator.negotiation.state_machine.resource.NegotiationResourceState;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.Objects;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 import org.springframework.hateoas.server.core.Relation;
 
-@Getter
-@Setter
+@Data
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Relation(collectionRelation = "organizations", itemRelation = "organization")
 @Schema(description = "An organization")
 public class OrganizationForNegotiationDTO extends OrganizationDTO {
   boolean updatable;
-
   NegotiationResourceState status;
-
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
-    OrganizationForNegotiationDTO that = (OrganizationForNegotiationDTO) o;
-    return updatable == that.updatable && status == that.status;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode(), updatable, status);
-  }
 }
