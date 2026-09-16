9e7754e32548cb4faff3b448a2777d4faf536eb5
Markus Stetschnig <markus.stetschnig@bbmri-eric.eu>
2026-09-16 13:26:49 +0200

    test(e2e): skip PDF export specs when the feature flag is off

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   chromeWebSecurity: false,
+  env: {
+    // eslint-disable-next-line no-undef
+    FEATURE_FLAG_PDF_EXPORT_ENABLED: process.env.FEATURE_FLAG_PDF_EXPORT_ENABLED === 'true'
+  },
   e2e: {
     setupNodeEvents() {
       // implement node event listeners here
