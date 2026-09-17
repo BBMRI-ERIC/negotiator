a92db1f7407c5eb74058896b26272bb53369fd25
Milos Terzic <tmilos94@gmail.com>
2026-09-17 13:53:12 +0200

    feat: store rewrite + router guard + main.js rewiring

--- frontend/src/store/oidc.js
+++ frontend/src/store/oidc.js
@@ -1,13 +1,55 @@
+import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
-import { piniaOidcCreateStoreModule } from 'pinia-oidc'
-import oidcSettings from '../config/oidc'
+import { userManager } from '../services/oidcUserManager'
 
-// Create the OIDC store module
-const oidcStoreModule = piniaOidcCreateStoreModule(
-  oidcSettings,
-  // Optional OIDC store settings
-  { removeUserWhenTokensExpire: false },
-)
+const ACTIVE_ROUTE_KEY = 'oidc_active_route'
 
-// Define the Pinia store using the OIDC store module
-export const useOidcStore = defineStore('oidc', oidcStoreModule)
+export const useOidcStore = defineStore('oidc', () => {
+  const user = ref(null)
+
+  const ready = userManager.getUser().then((loadedUser) => {
+    if (loadedUser && !loadedUser.expired) {
+      user.value = loadedUser
+    }
+  })
+
+  userManager.events.addUserLoaded((loadedUser) => {
+    user.value = loadedUser
+  })
+  userManager.events.addAccessTokenExpired(() => {
+    user.value = null
+  })
+
+  const oidcIsAuthenticated = computed(() => !!user.value && !user.value.expired)
+  const oidcUser = computed(() => user.value?.profile ?? null)
+  const access_token = computed(() => user.value?.access_token ?? null)
+
+  function authenticateOidc(redirectPath) {
+    if (redirectPath) {
+      sessionStorage.setItem(ACTIVE_ROUTE_KEY, redirectPath)
+    } else {
+      sessionStorage.removeItem(ACTIVE_ROUTE_KEY)
+    }
+    return userManager.signinRedirect()
+  }
+
+  function signOutOidc() {
+    return userManager.signoutRedirect()
+  }
+
+  function oidcSignInCallback() {
+    return userManager.signinRedirectCallback().then(() => {
+      return sessionStorage.getItem(ACTIVE_ROUTE_KEY) || '/'
+    })
+  }
+
+  return {
+    ready,
+    oidcIsAuthenticated,
+    oidcUser,
+    access_token,
+    authenticateOidc,
+    signOutOidc,
+    oidcSignInCallback,
+  }
+})
