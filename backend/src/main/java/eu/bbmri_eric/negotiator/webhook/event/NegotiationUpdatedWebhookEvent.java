141dd53d39712cc3226d335760733b53fe69c103
Ondrej Holis <ondrej.holis@sap.com>
2026-09-22 16:40:19 +0200

    fix: improve openapi example value

--- backend/src/main/java/eu/bbmri_eric/negotiator/webhook/event/NegotiationUpdatedWebhookEvent.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/webhook/event/NegotiationUpdatedWebhookEvent.java
@@ -12,5 +12,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
     description = "Sent when a negotiation was updated.")
 @Schema(description = "Webhook data payload for an updated negotiation.")
 record NegotiationUpdatedWebhookEvent(
-    @Schema(description = "Identifier of the affected negotiation", example = "negotiation-1")
+    @Schema(
+            description = "Identifier of the affected negotiation",
+            example = "29f1819b-a1d4-48b6-a284-2f9e269bbd05")
         String negotiationId) {}
