dd5cbc3498bff71c9fc9a7a256c512b095ed84b2
Markus Stetschnig <markus.stetschnig@bbmri-eric.eu>
2026-09-24 14:06:35 +0200

    refactor: add addResources method to negotiation

--- backend/src/main/java/eu/bbmri_eric/negotiator/governance/resource/ResourceServiceImpl.java
+++ backend/src/main/java/eu/bbmri_eric/negotiator/governance/resource/ResourceServiceImpl.java
@@ -104,8 +104,8 @@ public class ResourceServiceImpl implements ResourceService {
               && negotiation.getCurrentState().equals(NegotiationState.DRAFT))) {
         throw new ForbiddenRequestException("You are not allowed to perform this action");
       }
-      // Add one at a time: setResources would rebuild every link and clear its state.
-      resourcesToUpdate.forEach(negotiation::addResource);
+
+      negotiation.addResources(resourcesToUpdate);
     }
   }
 
